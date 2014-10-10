9babb1abd746b7f1f3f791ae1baae992644cb0f5
Michael Alexander <alexandermw@gmail.com>
2014-10-09 22:08:54 -0700

    category pages complete

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -26,12 +26,30 @@ class CategoriesController < ApplicationController
 
   def edit
     @category = Category.find(params[:id])
+
   end
 
   def update
+    @category = Category.find(params[:id])
+
+    if @category.update(whitelisted_params)
+      flash[:success] = "Updated!"
+      redirect_to action: :index
+    else
+      flash.now[:error] = "Something went wrong."
+      render :edit
+    end
   end
 
   def destroy
+    session[:return_to] ||= request.referer
+    if Category.find(params[:id]).destroy
+      flash[:success] = "Deleted."
+      redirect_to action: :index
+    else
+      flash[:error] = "Something went wrong in that deletion."
+      redirect_to session.delete[:return_to]
+    end
   end
 
 private
