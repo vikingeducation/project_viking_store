6c11f10f0603a63c3a5f90a740df835a5c7169bf
Paul Hyman <mail@phyman.com>
2014-10-11 15:24:38 -0500

    finished category update/delete functionality

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -8,7 +8,7 @@ class CategoriesController < ApplicationController
     @category = Category.find(params[:id])
   end
 
-  def new 
+  def new
     @category = Category.new
   end
 
@@ -17,6 +17,9 @@ class CategoriesController < ApplicationController
   end
 
   def destroy
+    Category.find(params[:id]).destroy
+    flash[:success] = "Category deleted."
+    redirect_to categories_path
   end
 
   def create
@@ -26,17 +29,32 @@ class CategoriesController < ApplicationController
       redirect_to categories_path
     else
       flash[:error] = "Whoops!"
-      render 'new' 
+      render 'new'
     end
   end
 
   def update
-
+    @category = Category.find(params[:id])
+    if confirm_category( @category.id ) # TODO - put this in the model as callback (maybe)
+      if @category.update(cat_params)
+        flash[:success] = "Category edited!"
+        redirect_to @category
+      else
+        flash[:error] = "Whoops!"
+        render 'edit'
+      end
+    else
+      render 'edit'
+    end
   end
 
   private
   def cat_params
     params.require(:category).permit(:name, :description)
+  end
+
+  def confirm_category(cat_num)
+    params[:id] == cat_num.to_s
   end
 
 end
