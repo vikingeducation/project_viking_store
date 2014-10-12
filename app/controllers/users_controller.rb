2caf7af71544769ba201def47f7b4324173685e9
Tom McLaughlin <tim5046@gmail.com>
2014-10-12 12:50:13 -0400

    finished new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,4 +9,33 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     render :layout => "admin_interface"
   end
+
+  def new
+    @user = User.new
+    render :layout => "admin_interface"
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "New User \"#{@user.first_name} #{@user.last_name}\" has been successfully created!"
+      redirect_to users_path
+    else
+      flash[:error] = "User was not created, please try again"
+      render 'new'
+    end
+  end
+
+
+  # ---- utility methods ----
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name,
+         :email, :phone, :default_billing_address_id,
+         :default_shipping_address_id)
+  end
+
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,13 @@ class User < ActiveRecord::Base
 
   has_one :payment
 
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+
+  validates :first_name, presence: true, length: {minimum: 1, maximum:64}
+  validates :last_name, presence: true, length: {minimum: 1, maximum:64}
+  validates :email, presence: true, length: {minimum: 1, maximum:64},
+            format: { with: VALID_EMAIL_REGEX }
+
   def self.user(time)
     User.where('created_at > ?',time).count
   end
