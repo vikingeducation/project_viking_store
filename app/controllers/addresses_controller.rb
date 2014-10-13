fdeddd2a6efd6ae9a60fb5f49d6738ff3debf7d2
Paul Hyman <mail@phyman.com>
2014-10-12 19:27:23 -0500

    filtering addresses based on user and defaulting to address index if user id is out of range

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,15 +1,30 @@
 class AddressesController < ApplicationController
   def index
-    @addresses=Address.all
+    if (params[:user_id] != nil)
+      confirm_user_id
+    else
+      @addresses=Address.all
+    end
     render :layout => "admin_interface"
   end
+
   def show
     @address = Address.find(params[:id])
     render :layout => "admin_interface"
   end
-  
+
   def new
     @address = Address.new
     render :layout => "admin_interface"
+  end
+end
+
+def confirm_user_id
+  if Address.where(id: params[:user_id]).empty?
+      @addresses=Address.all
+  else
+      @addresses=Address.where(user_id: params[:user_id])
+      @filtered = true
+      @params_user_id = params[:user_id]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ root 'admin#index'
   resources :users
   resources :addresses
 
+  get 'addresses/params[:user_id]' => 'addresses#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
