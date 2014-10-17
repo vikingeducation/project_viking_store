22a7e26bb993dd58091b659d3a4c882004f4bf3f
Michael Alexander <alexandermw@gmail.com>
2014-10-16 18:03:12 -0700

    add more login helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,22 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+
+  private
+
+  def require_current_user
+    unless current_user == User.find(session[:id])
+      flash[:error] = "Access denied!!!"
+      redirect_to root_url
+    end
+  end
+
+  def require_login
+    unless signed_in_user?
+      flash[:error] = "You need to sign in to see this."
+      redirect_to login_path
+    end
+  end
+
 end
