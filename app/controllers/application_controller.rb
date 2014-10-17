59bc988cd3bcfb93bc81491e994299367106e658
Michael Alexander <alexandermw@gmail.com>
2014-10-17 08:33:17 -0700

    current user persists across multiple sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_action :check_current_user
 
   def sign_in(user)
     session[:current_user_id] = user.id
@@ -34,6 +35,10 @@ class ApplicationController < ActionController::Base
      @current_user = user
    end
    
+  private
 
+  def check_current_user
+    @current_user = current_user
+  end
 
 end
