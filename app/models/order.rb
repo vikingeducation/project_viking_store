3d45de5c68f38112711aed2005f4b251efa6d21b
Daniel Au <dakmau@gmail.com>
2015-03-01 04:01:30 -0500

    Add second panel analytics

--- app/models/order.rb
+++ app/models/order.rb
@@ -7,8 +7,12 @@ class Order < ActiveRecord::Base
     end
   end
 
-  def self.total_revenue(time_period)
-
+  def self.total_revenue(number_of_days=nil)
+    if number_of_days.nil?
+    Order.select("SUM(quantity * price) AS revenue").joins("JOIN order_contents ON orders.id = order_contents.order_id JOIN products ON products.id = order_contents.product_id").where("checkout_date IS NOT NULL").first.revenue
+    else
+      Order.select("SUM(quantity * price) AS revenue").joins("JOIN order_contents ON orders.id = order_contents.order_id JOIN products ON products.id = order_contents.product_id").where("checkout_date > ?", number_of_days).first.revenue
+    end
   end
 
   def self.average_order_value(time_period)
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,10 @@
 class Product < ActiveRecord::Base
-  def self.new_products_count(time_period)
+  def self.number_of_products(number_of_days=nil)
+    if number_of_days.nil?
+      Product.where("created_at IS NOT NULL").count
+    else
+      Product.where("created_at > ?", number_of_days.days.ago).count
+    end
   end
+
 end
