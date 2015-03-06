455a70dfa24ef506a927a71969081977a41bf037
Dan Berger <dsberger@gmail.com>
2015-03-06 13:44:19 -0800

    order.rb updates

--- app/models/order.rb
+++ app/models/order.rb
@@ -9,14 +9,14 @@ class Order < ActiveRecord::Base
   def self.revenue_in_the_last_seven_days
     Order.select("SUM(quantity * price) AS revenue").
       joins("JOIN order_contents ON orders.id = order_contents.order_id JOIN products ON products.id = order_contents.product_id").
-      where("checkout_date IS NOT NULL AND checkout_date > ?", 7.days.ago).
+      where("checkout_date > ?", 7.days.ago).
       first.revenue
   end
 
   def self.revenue_in_the_last_thirty_days
     Order.select("SUM(quantity * price) AS revenue").
       joins("JOIN order_contents ON orders.id = order_contents.order_id JOIN products ON products.id = order_contents.product_id").
-      where("checkout_date IS NOT NULL AND checkout_date > ?", 30.days.ago).
+      where("checkout_date > ?", 30.days.ago).
       first.revenue
   end
 
@@ -84,14 +84,14 @@ class Order < ActiveRecord::Base
   def self.quantity_of_orders_last_week(weeks_ago)
     Order.select("COUNT(*) AS weeks_orders, SUM(quantity * price) AS weeks_value").
       joins("JOIN order_contents ON orders.id = order_contents.order_id JOIN products ON products.id = order_contents.product_id").
-      where("checkout_date IS NOT NULL AND checkout_date BETWEEN ? AND ?", (weeks_ago+1).weeks.ago, weeks_ago.weeks.ago).
+      where("checkout_date BETWEEN ? AND ?", (weeks_ago+1).weeks.ago, weeks_ago.weeks.ago).
       first
   end
 
   def self.quantity_of_orders_by_day(days_ago)
     Order.select("COUNT(*) AS days_orders, SUM(quantity * price) AS days_value").
       joins("JOIN order_contents ON orders.id = order_contents.order_id JOIN products ON products.id = order_contents.product_id").
-      where("checkout_date IS NOT NULL AND checkout_date BETWEEN ? AND ?", (days_ago+1).days.ago, days_ago.days.ago).
+      where("checkout_date BETWEEN ? AND ?", (days_ago+1).days.ago, days_ago.days.ago).
       first
   end
 end
