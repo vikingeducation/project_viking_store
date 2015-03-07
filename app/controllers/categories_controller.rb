88b4b6694d3ffa3d92873dbbb4ff622335795402
Dan Berger <dsberger@gmail.com>
2015-03-07 00:10:57 -0800

    some new Category business

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,4 +4,25 @@ class CategoriesController < ApplicationController
     @categories = Category.all
     render layout: "admin"
   end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new whitelisted_category_params
+    if @category.save
+      flash[:success] = "You created a new category."
+      redirect_to categories_path
+    else
+      flash[:error] = "There was an error."
+      render :new
+    end
+  end
+
+  private
+
+  def whitelisted_category_params
+    params.require(:category).permit(:name,:description)
+  end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,7 @@ class Category < ActiveRecord::Base
 
   has_many :products, dependent: :destroy
 
+  validates :name,
+            presence: true,
+            length: { in: 4..16 }
 end
