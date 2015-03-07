d9ba776cea61a40add2e7344a1d1ea0ff776af8b
Dan Berger <dsberger@gmail.com>
2015-03-07 11:36:34 -0800

    added Show and Edit to Products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,4 +3,24 @@ class ProductsController < ApplicationController
     @products = Product.all
     render layout: "admin"
   end
+
+  def show
+    @product = Product.find(params[:id])
+    render layout: "admin"
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+    @categories = Category.all
+    @list_of_category_names = list_of_category_names
+    render layout: "admin"
+  end
+
+  private
+
+  def list_of_category_names
+    Category.all.each_with_object([]) do |cat, list|
+      list << cat.name
+    end
+  end
 end
