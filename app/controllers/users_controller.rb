f3a851840d869b30945e4f706b6a11708da9497c
Daniel Au <dakmau@gmail.com>
2015-03-07 19:14:41 -0500

    Finish User

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,10 @@
 class UsersController < ApplicationController
-
   def index
     @users = User.all
     render layout: "admin"
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,11 @@
 module UsersHelper
+  def billing_address_for user
+    address = user.billing_address
+    "#{address.street_address}, #{address.city.name}, #{address.state.name} #{address.zip_code}"
+  end
+
+  def shipping_address_for user
+    address = user.shipping_address
+    "#{address.street_address}, #{address.city.name}, #{address.state.name} #{address.zip_code}"
+  end
 end
