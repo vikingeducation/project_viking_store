f763e165c9cb90bdd107ee845683b081e1f4c35e
telwell <me@trevorelwell.com>
2015-06-08 22:05:10 -0400

    Final solution, needs refactoring

--- app/models/order.rb
+++ app/models/order.rb
@@ -27,12 +27,22 @@ class Order < ActiveRecord::Base
 
 	# Highest single order value.
 	def self.highest_order_value
-		select("users.first_name AS first_name, users.last_name AS last_name, SUM(products.price * order_contents.quantity) AS order_price").
-		joins("JOIN users ON users.id = orders.user_id JOIN order_contents ON orders.id = order_contents.order_id JOIN products ON order_contents.product_id = products.id").
-		where("orders.checkout_date IS NOT null").
-		group("order_contents.order_id").
-		order("order_price DESC").
-		limit(1).first
+		if block_given?
+			in_timeframe = yield
+			in_timeframe.select("users.first_name AS first_name, users.last_name AS last_name, SUM(products.price * order_contents.quantity) AS order_price").
+			joins("JOIN users ON users.id = orders.user_id JOIN order_contents ON orders.id = order_contents.order_id JOIN products ON order_contents.product_id = products.id").
+			where("orders.checkout_date IS NOT null").
+			group("order_contents.order_id").
+			order("order_price DESC").
+			limit(1).first
+		else
+			select("users.first_name AS first_name, users.last_name AS last_name, SUM(products.price * order_contents.quantity) AS order_price").
+			joins("JOIN users ON users.id = orders.user_id JOIN order_contents ON orders.id = order_contents.order_id JOIN products ON order_contents.product_id = products.id").
+			where("orders.checkout_date IS NOT null").
+			group("order_contents.order_id").
+			order("order_price DESC").
+			limit(1).first
+		end
 	end
 
 	# Highest lifetime order value
@@ -62,6 +72,40 @@ class Order < ActiveRecord::Base
 		where("orders.checkout_date IS NOT null").
 		group("users.id").
 		order("orders DESC").
+		limit(1).first
+	end
+
+	# Average value of all orders w/ timeframe support
+	def self.average_order_value(timeframe = nil)
+			if timeframe.nil?
+				average_stats = select("SUM(products.price * order_contents.quantity) AS order_value, COUNT(DISTINCT order_contents.order_id) AS number_of_orders").
+				joins("JOIN users ON users.id = orders.user_id JOIN order_contents ON orders.id = order_contents.order_id JOIN products ON order_contents.product_id = products.id").
+				where("orders.checkout_date IS NOT null").limit(1).first
+				average_stats.order_value / average_stats.number_of_orders
+			else
+				average_stats = select("SUM(products.price * order_contents.quantity) AS order_value, COUNT(DISTINCT order_contents.order_id) AS number_of_orders").
+				joins("JOIN users ON users.id = orders.user_id JOIN order_contents ON orders.id = order_contents.order_id JOIN products ON order_contents.product_id = products.id").
+				where("orders.checkout_date IS NOT null").where("orders.checkout_date >= ?", timeframe).limit(1).first
+				average_stats.order_value / average_stats.number_of_orders
+			end	
+	end
+
+	# FINALLY the last method here is going to be order_by_date
+	# which will take a date parameter and return the orders 
+	# value of those orders based on the date.
+	def self.order_by_day(date)
+		select("COUNT(DISTINCT orders.id) AS order_quantity, SUM(order_contents.quantity * products.price) AS order_value").
+		joins("JOIN order_contents ON orders.id = order_contents.order_id JOIN products ON order_contents.product_id = products.id").
+		where("orders.checkout_date IS NOT null").
+		where("orders.checkout_date BETWEEN ? AND ?", date.beginning_of_day, date.end_of_day).
+		limit(1).first
+	end
+
+	def self.order_by_week(start_date, end_date)
+		select("COUNT(DISTINCT orders.id) AS order_quantity, SUM(order_contents.quantity * products.price) AS order_value").
+		joins("JOIN order_contents ON orders.id = order_contents.order_id JOIN products ON order_contents.product_id = products.id").
+		where("orders.checkout_date IS NOT null").
+		where("orders.checkout_date BETWEEN ? AND ?", start_date.beginning_of_day, end_date.end_of_day).
 		limit(1).first
 	end
 end
