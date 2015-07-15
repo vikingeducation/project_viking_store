8c13467362266a035d4b5762aef56dd34398f1d6
Adam Kinson <adam.kinson@gmail.com>
2015-07-15 12:08:24 -0400

    complete panel 1 queries & view

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,15 +1,12 @@
 class DashboardController < ApplicationController
 
-  def index
-    user_count = User.count_new_users(7)
-    order_count = Order.count_orders(7)
-    new_products = Product.count_new_products(7)
-    revenue = Order.calc_revenue(7)
+  include DashboardHelper
 
-    @data = { 'New Users' => user_count,
-              'Orders' => order_count,
-              'New Products' => new_products,
-              'Revenue' => revenue
+  def index
+
+    @data = { 'Last 7 Days' => aggregate_data(7),
+              'Last 30 Days' => aggregate_data(30),
+              'Total' => aggregate_data
             }
   end
 
--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,2 +1,16 @@
 module DashboardHelper
+
+  def aggregate_data(day_range = nil)
+
+    output = {'New Users' => User.count_new_users(day_range),
+              'Orders' => Order.count_orders(day_range),
+              'New Products' => Product.count_new_products(day_range),
+              'Revenue' => Order.calc_revenue(day_range)
+              }
+
+    output
+
+  end
+
+
 end
