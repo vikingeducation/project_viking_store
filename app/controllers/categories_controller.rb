fdd5912baf062214e0fde7bf8520e9966fc69658
Adam <adam.kinson@gmail.com>
2015-07-19 10:54:24 -0400

    build basic Category index structure

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,7 @@
+class CategoriesController < ApplicationController
+
+  def index
+    render :layout => 'portal'
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
 
   get 'dashboard' => 'dashboard#index'
 
+
+  resources :categories, :only => [:index]
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
