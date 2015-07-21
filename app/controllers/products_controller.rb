ecb02ef52f80759ae8a4619d72f178b3f9b17e48
Adam <adam.kinson@gmail.com>
2015-07-20 23:10:17 -0400

    finish Product section

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -23,11 +23,17 @@ class ProductsController < ApplicationController
   def create
     @product = Product.new(product_params)
 
+    @product.price = format_price(params[:product][:price])
+
+    @product.sku = Product.generate_new_sku
+
+
     if @product.save
       flash[:success] = "Product successfully created!"
       redirect_to products_path
     else
       flash.now[:danger] = "Product not saved - please try again."
+      @available_categories = Category.list_all_categories
       render :new
     end
 
@@ -36,17 +42,21 @@ class ProductsController < ApplicationController
 
   def edit
     @product = Product.find(params[:id])
+    @available_categories = Category.list_all_categories
   end
 
 
   def update
     @product = Product.find(params[:id])
 
+    # check if ID is valid to be edited???
+
     if @product.update(product_params)
       flash[:success] = "Product successfully updated!"
       redirect_to products_path
     else
       flash.now[:danger] = "Product not saved - please try again."
+      @available_categories = Category.list_all_categories
       render :edit
     end
   end
@@ -69,8 +79,18 @@ class ProductsController < ApplicationController
   private
 
 
-  def category_params
-    params.require(:product).permit(:id, :name, :price, :category)
+  # Silently disallows changes to Product ID, but is there a way (above) to throw an error?
+  def product_params
+    params.require(:product).permit(:name, :price, :category_id)
+  end
+
+
+  def format_price(price_param)
+    if price_param.match(/\$.+/)
+      price_param[1..-1].to_f
+    else
+      price_param.to_f
+    end
   end
 
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -3,8 +3,9 @@ class Product < ActiveRecord::Base
   has_many :order_contents, :class_name => "OrderContents"
   has_many :orders, :through => :order_contents
 
-  validates :name, :price, :category, :presence => true
-  validates :price, :numericality => true
+  validates :name, :price, :category, :sku, :presence => true
+  validates :price, :numericality => true, :length =>{ :in => 0..10_000 }
+
 
   def self.count_new_products(day_range = nil)
     if day_range.nil?
@@ -12,6 +13,26 @@ class Product < ActiveRecord::Base
     else
       Product.where("created_at > ?", Time.now - day_range.days).count
     end
+  end
+
+
+  def self.generate_new_sku
+    new_sku = (Faker::Code.ean).to_i
+
+    while Product.exists?(:sku => new_sku)
+      new_sku = (Faker::Code.ean).to_i
+    end
+
+    new_sku
+  end
+
+
+
+  private
+
+
+  def self.list_all_skus
+    Product.select(:sku).order(:sku)
   end
 
 
