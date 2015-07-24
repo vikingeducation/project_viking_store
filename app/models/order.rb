2025d36b8269ea22dd6cba1fb9cf69f04c0ed09e
ayva <olga.ayva@gmail.com>
2015-07-24 11:01:34 -0700

    merge conflict

--- app/models/order.rb
+++ app/models/order.rb
@@ -10,7 +10,8 @@ class Order < ActiveRecord::Base
     prod_all=Product.where("created_at > ?",Time.now - 7.day)
     prod_num=prod_all.count
 
-    revenue = Order.joins("JOIN order_contents ON orders.id = order_id").joins("JOIN products ON products.id = product_id").select("SUM(price * quantity) AS revenue").where("checkout_date > ?",Time.now - 7.day).first
+    revenue_table = Order.joins("JOIN order_contents ON orders.id = order_id").joins("JOIN products ON products.id = product_id").select("SUM(price * quantity) AS revenue").where("checkout_date > ?",Time.now - 7.day)
+    revenue=revenue_table.first.revenue
 
     @table_data={"New Users" => user_num, "Orders"=>order_num, "New Products" => prod_num, "Revenue" => revenue}
 
