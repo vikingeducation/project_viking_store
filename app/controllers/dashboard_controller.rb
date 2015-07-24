981c176b8aed9dc56f5faa472472fab6b8f4ff9b
shadefinale <shadefinale@gmail.com>
2015-07-24 14:13:30 -0500

    Finished 2nd dashboard section.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,24 +2,10 @@ class DashboardController < ApplicationController
 
   def index
     # Assign arrays to the hash based on the starting values for the hash.
-    @overall = {'Last 7' => 7, 'Last 30' => 30, 'Total' => nil}
-    @overall.each do |key, limit|
-      result = []
-      result << User.in_last(limit)
-      result << Order.in_last(limit)
-      result << Product.in_last(limit)
-      result << Order.revenue_in_last(limit)
-      @overall[key] = result
-    end
+    @overall = User.get_overall
 
-    @demographics = {'Top 3 states' => [], 'Top 3 cities' => [] }
-    @demographics.each do |key|
-      top_states = User.top_states
-      p top_states
-      top_states.each do |state|
-        p state
-        @demographics[key] << [state.name, state.total]
-      end
+    @demographics = User.get_demographics
+
+    @superlatives = User.get_superlatives
     end
-  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,47 +8,91 @@ class User < ActiveRecord::Base
     end
   end
 
-  def self.top_states(limit = 3)
-    self.select('states.name, COUNT(addresses.state_id) AS total').
-    joins('JOIN addresses ON users.billing_id = addresses.id JOIN states ON states.id = addresses.state_id').
-    group('states.name').order('COUNT(addresses.state_id) DESC').
-    limit(limit)
+  def self.get_overall
+    overall = {'Last 7' => 7, 'Last 30' => 30, 'Total' => nil}
+    overall.each do |key, limit|
+      result = []
+      result << User.in_last(limit)
+      result << Order.in_last(limit)
+      result << Product.in_last(limit)
+      result << Order.revenue_in_last(limit)
+      overall[key] = result
+    end
+    overall
   end
 
-  def self.top_cities(limit = 3)
-    self.select('cities.name, COUNT(addresses.city_id) AS total').
-    joins('JOIN addresses ON users.billing_id = addresses.id JOIN cities ON cities.id = addresses.city_id').
-    group('cities.name').order('COUNT(addresses.city_id) DESC').
-    limit(limit)
+  def self.get_demographics
+    demographics = {'Top 3 states' => [], 'Top 3 cities' => [] }
+    self.top_states.each do |state|
+      demographics['Top 3 states'] << [state.name, state.total]
+    end
+
+    self.top_cities.each do |state|
+      demographics['Top 3 cities'] << [state.name, state.total]
+    end
+    demographics
   end
 
-  def self.high_single_order_value
-    self.select('users.first_name || users.last_name AS full_name, SUM(products.price * order_contents.quantity) as cost').
-    joins('JOIN orders ON users.id = orders.user_id JOIN order_contents ON order_contents.order_id = orders.id JOIN products ON order_contents.product_id = products.id').
-    where("orders.checkout_date NOT NULL").
-    group('orders.id').order('cost DESC').limit(1)
+  def self.get_superlatives
+    superlatives = {}
+    highest_s = User.high_single_order_value.first
+    superlatives['Highest Single Order Value'] = [highest_s.full_name,
+                                                   highest_s.cost]
+    highest_l = User.high_lifetime_value.first
+    superlatives['Highest Lifetime Value'] = [highest_l.full_name,
+                                               highest_l.cost]
+    highest_a = User.high_average_value.first
+    superlatives['Highest Average Order Value'] = [highest_a.full_name,
+                                                    highest_a.average_value]
+    m_orders = User.most_orders.first
+    superlatives['Most Orders Placed'] = [m_orders.full_name,
+                                           m_orders.number_of_orders]
+    return superlatives
   end
 
-  def self.high_lifetime_value
-    self.select('users.first_name || users.last_name AS full_name, SUM(products.price * order_contents.quantity) as cost').
-    joins('JOIN orders ON users.id = orders.user_id JOIN order_contents ON order_contents.order_id = orders.id JOIN products ON order_contents.product_id = products.id').
-    where("orders.checkout_date NOT NULL").
-    group('users.id').order('cost DESC').limit(1)
-  end
+  private
 
-  def self.high_average_value
-    self.select('users.first_name || users.last_name AS full_name, SUM(products.price * order_contents.quantity)/(COUNT(DISTINCT orders.id)) as average_value').
-    joins('JOIN orders ON users.id = orders.user_id JOIN order_contents ON order_contents.order_id = orders.id JOIN products ON order_contents.product_id = products.id').
-    where("orders.checkout_date NOT NULL").
-    group('users.id').order('average_value DESC').limit(1)
-  end
+    def self.top_states(limit = 3)
+      self.select('states.name, COUNT(addresses.state_id) AS total').
+      joins('JOIN addresses ON users.billing_id = addresses.id JOIN states ON states.id = addresses.state_id').
+      group('states.name').order('COUNT(addresses.state_id) DESC').
+      limit(limit)
+    end
 
-  def self.most_orders
-    self.select('users.first_name || users.last_name AS full_name, COUNT(orders.id) as number_of_orders').
-    joins('JOIN orders ON users.id = orders.user_id').
-    where("orders.checkout_date NOT NULL").
-    group('users.id').order('number_of_orders DESC').limit(1)
-  end
+    def self.top_cities(limit = 3)
+      self.select('cities.name, COUNT(addresses.city_id) AS total').
+      joins('JOIN addresses ON users.billing_id = addresses.id JOIN cities ON cities.id = addresses.city_id').
+      group('cities.name').order('COUNT(addresses.city_id) DESC').
+      limit(limit)
+    end
+
+    def self.high_single_order_value
+      self.select('users.first_name || users.last_name AS full_name, SUM(products.price * order_contents.quantity) as cost').
+      joins('JOIN orders ON users.id = orders.user_id JOIN order_contents ON order_contents.order_id = orders.id JOIN products ON order_contents.product_id = products.id').
+      where("orders.checkout_date NOT NULL").
+      group('orders.id').order('cost DESC').limit(1)
+    end
+
+    def self.high_lifetime_value
+      self.select('users.first_name || users.last_name AS full_name, SUM(products.price * order_contents.quantity) as cost').
+      joins('JOIN orders ON users.id = orders.user_id JOIN order_contents ON order_contents.order_id = orders.id JOIN products ON order_contents.product_id = products.id').
+      where("orders.checkout_date NOT NULL").
+      group('users.id').order('cost DESC').limit(1)
+    end
+
+    def self.high_average_value
+      self.select('users.first_name || users.last_name AS full_name, SUM(products.price * order_contents.quantity)/(COUNT(DISTINCT orders.id)) as average_value').
+      joins('JOIN orders ON users.id = orders.user_id JOIN order_contents ON order_contents.order_id = orders.id JOIN products ON order_contents.product_id = products.id').
+      where("orders.checkout_date NOT NULL").
+      group('users.id').order('average_value DESC').limit(1)
+    end
+
+    def self.most_orders
+      self.select('users.first_name || users.last_name AS full_name, COUNT(orders.id) as number_of_orders').
+      joins('JOIN orders ON users.id = orders.user_id').
+      where("orders.checkout_date NOT NULL").
+      group('users.id').order('number_of_orders DESC').limit(1)
+    end
 
 
 end
