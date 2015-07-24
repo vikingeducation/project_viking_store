dc565eba0895801b76760669847688e03dfe7a17
Nick Sarlo <nicholas.l.sarlo@gmail.com>
2015-07-24 11:55:52 -0500

    created html, first queries

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,11 @@
 class Order < ActiveRecord::Base
+
+  def order_count(timeframe = nil)
+
+    if timeframe.nil?
+      return Order.all.length
+    else
+      return Order.where("created_at > ?", timeframe.days.ago).count
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,16 @@
 class User < ActiveRecord::Base
+
+  # 1. Overall Platform
+
+  # Last 7 Days
+
+  def user_count(timeframe = nil)
+
+    if timeframe.nil?
+      return User.all.length
+    else
+      return User.where("created_at > ?", timeframe.days.ago).count
+    end
+  end
+
 end
