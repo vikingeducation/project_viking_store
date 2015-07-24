64cf87f2bd417a57b2cad0eafcf171d3a9e63722
David <davidmeza@outlook.com>
2015-07-24 13:18:54 -0400

    add highest single order value

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,22 @@ class User < ActiveRecord::Base
   end
 
   def self.top_states(limit = 3)
-    self.joins('JOIN address ON users.billing_id = address.id').count('')
+    self.select('states.name, COUNT(addresses.state_id)').
+    joins('JOIN addresses ON users.billing_id = addresses.id JOIN states ON states.id = addresses.state_id').
+    group('states.name').order('COUNT(addresses.state_id) DESC').
+    limit(limit)
   end
+
+  def self.top_cities(limit = 3)
+    self.select('cities.name, COUNT(addresses.city_id)').
+    joins('JOIN addresses ON users.billing_id = addresses.id JOIN cities ON cities.id = addresses.city_id').
+    group('cities.name').order('COUNT(addresses.city_id) DESC').
+    limit(limit)
+  end
+
+  def self.high_single_order_value
+    self.select('users.first_name, SUM(products.price * order_contents.quantity) as cost').joins('JOIN orders ON users.id = orders.user_id JOIN order_contents ON order_contents.order_id = orders.id JOIN products ON order_contents.product_id = products.id').group('orders.id').order('cost DESC').limit(1)
+  end
+
 
 end
