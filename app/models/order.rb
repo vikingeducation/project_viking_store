36a363b99c8093701324fc0d23dd19453ba1de77
alokp <alokpra@gmail.com>
2015-07-24 11:30:16 -0700

    rendering user demographics

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,5 +4,19 @@ def self.new_orders(input_day)
 	self.where("created_at > ?", input_day.days.ago).count
 end
 
+def self.revenue_table(input)
+  	# if input is given:
+    table = self.where("checkout_date > ?", input.days.ago).joins("JOIN order_contents ON orders.id = order_contents.order_id").joins("JOIN products ON products.id = order_contents.product_id") if input
+    # if no input, get all orders with checkout date
+    table = self.where("checkout_date IS NOT NULL").joins("JOIN order_contents ON orders.id = order_contents.order_id").joins("JOIN products ON products.id = order_contents.product_id") unless input
+    return table
+end
+
+def self.revenue(input=nil)
+	table = revenue_table(input).select(:order_id, :quantity, :product_id, :price)
+	revenue = table.select("round(SUM(quantity * price), 2) AS sum")
+	revenue.first[:sum]
+end
+
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,18 @@ def self.new_users(input_day)
 	self.where("created_at > ?", input_day.days.ago).count
 end
 
+def self.get_top_states
+ 	result = []
+ 	table = Order.joins("JOIN addresses ON orders.billing_id = addresses.id")
+ 	top_states = table.select("addresses.state_id, count(*) AS users").group("addresses.state_id").order("users desc").limit(3)
+ 	table = top_states.joins("JOIN states ON state_id = states.id")
+ 	table.select("states.name")
+ 	result = [
+			 	[table.select("states.name").first[:name], table.select("states.name").first[:users]],
+			 	[table.select("states.name").second[:name], table.select("states.name").second[:users]],
+			 	[table.select("states.name").third[:name], table.select("states.name").third[:users]]
+ 			]
+end
+
 
 end
