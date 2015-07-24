428ef9563eb3a029a1e6ae95ebd126d9ba4054f9
mikebutts <mgbutts@gmail.com>
2015-07-24 13:03:35 -0400

    doing thangs

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,6 @@
 class Order < ActiveRecord::Base
+
+  def self.revenue
+    Order.find_by_sql("SELECT Round(SUM(price),2) as price from order_contents JOIN products ON products.id = product_id")
+  end
 end
