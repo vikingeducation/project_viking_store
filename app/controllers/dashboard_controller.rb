f31ddcd66d900c047f54c6af7cd66e5d5738dbba
David <davidmeza@outlook.com>
2015-07-24 14:30:27 -0400

    working on dashboard stats

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,14 +2,24 @@ class DashboardController < ApplicationController
 
   def index
     # Assign arrays to the hash based on the starting values for the hash.
-    @results = {'Last 7' => 7, 'Last 30' => 30, 'Total' => nil}
-    @results.each do |key, limit|
+    @overall = {'Last 7' => 7, 'Last 30' => 30, 'Total' => nil}
+    @overall.each do |key, limit|
       result = []
       result << User.in_last(limit)
       result << Order.in_last(limit)
       result << Product.in_last(limit)
       result << Order.revenue_in_last(limit)
-      @results[key] = result
+      @overall[key] = result
+    end
+
+    @demographics = {'Top 3 states' => [], 'Top 3 cities' => [] }
+    @demographics.each do |key|
+      top_states = User.top_states
+      p top_states
+      top_states.each do |state|
+        p state
+        @demographics[key] << [state.name, state.total]
+      end
     end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,35 +9,45 @@ class User < ActiveRecord::Base
   end
 
   def self.top_states(limit = 3)
-    self.select('states.name, COUNT(addresses.state_id)').
+    self.select('states.name, COUNT(addresses.state_id) AS total').
     joins('JOIN addresses ON users.billing_id = addresses.id JOIN states ON states.id = addresses.state_id').
     group('states.name').order('COUNT(addresses.state_id) DESC').
     limit(limit)
   end
 
   def self.top_cities(limit = 3)
-    self.select('cities.name, COUNT(addresses.city_id)').
+    self.select('cities.name, COUNT(addresses.city_id) AS total').
     joins('JOIN addresses ON users.billing_id = addresses.id JOIN cities ON cities.id = addresses.city_id').
     group('cities.name').order('COUNT(addresses.city_id) DESC').
     limit(limit)
   end
 
   def self.high_single_order_value
-    self.select('users.first_name, users.last_name, SUM(products.price * order_contents.quantity) as cost').
+    self.select('users.first_name || users.last_name AS full_name, SUM(products.price * order_contents.quantity) as cost').
     joins('JOIN orders ON users.id = orders.user_id JOIN order_contents ON order_contents.order_id = orders.id JOIN products ON order_contents.product_id = products.id').
+    where("orders.checkout_date NOT NULL").
     group('orders.id').order('cost DESC').limit(1)
   end
 
   def self.high_lifetime_value
-    self.select('users.first_name, users.last_name, SUM(products.price * order_contents.quantity) as cost').
+    self.select('users.first_name || users.last_name AS full_name, SUM(products.price * order_contents.quantity) as cost').
     joins('JOIN orders ON users.id = orders.user_id JOIN order_contents ON order_contents.order_id = orders.id JOIN products ON order_contents.product_id = products.id').
+    where("orders.checkout_date NOT NULL").
     group('users.id').order('cost DESC').limit(1)
   end
 
   def self.high_average_value
-    self.select('users.first_name, users.last_name, SUM(products.price * order_contents.quantity)/(COUNT(orders.id)  - 1) as cost').
+    self.select('users.first_name || users.last_name AS full_name, SUM(products.price * order_contents.quantity)/(COUNT(DISTINCT orders.id)) as average_value').
     joins('JOIN orders ON users.id = orders.user_id JOIN order_contents ON order_contents.order_id = orders.id JOIN products ON order_contents.product_id = products.id').
-    group('users.id').order('cost DESC').limit(100)
+    where("orders.checkout_date NOT NULL").
+    group('users.id').order('average_value DESC').limit(1)
+  end
+
+  def self.most_orders
+    self.select('users.first_name || users.last_name AS full_name, COUNT(orders.id) as number_of_orders').
+    joins('JOIN orders ON users.id = orders.user_id').
+    where("orders.checkout_date NOT NULL").
+    group('users.id').order('number_of_orders DESC').limit(1)
   end
 
 
