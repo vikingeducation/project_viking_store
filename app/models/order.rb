8b2ecce0009e98b0a8ecdd21d6be80e5eb1d7c3a
facingsouth <xin.d.liu@gmail.com>
2015-07-24 16:49:32 -0700

    fix last 7 weeks

--- app/models/order.rb
+++ app/models/order.rb
@@ -40,31 +40,21 @@ class Order < ActiveRecord::Base
     # if scope == 'days'
     # t = 7
     Order.select("ROUND(SUM(quantity * products.price), 2) AS total, 
-                  DATE(checkout_date) as d, 
-                  current_date as cd,
-                  EXTRACT(DAY FROM (current_date - DATE(checkout_date))) AS wk")
+                  DATE(checkout_date) as d")
        .joins("JOIN order_contents ON order_contents.order_id=orders.id")
        .joins("JOIN products ON order_contents.product_id=products.id")
        .where("checkout_date IS NOT NULL AND checkout_date > ?", 49.days.ago)
-       .group("d, wk, cd")
+       .group("d")
   end
 
   def self.last_seven_weeks
 
-    # elsif scope == 'weeks'
-      # t = 49
-    week_total = []
-    7.times do |i|
-      start_date = ((i+1)*7).days.ago
-      end_date = ((i+1)*7+6).days.ago
-      week_total << Order.select("ROUND(SUM(quantity * products.price), 2) AS total, 
-                   (current_date - DATE(checkout_date)) AS wk")
-         .joins("JOIN order_contents ON order_contents.order_id=orders.id")
-         .joins("JOIN products ON order_contents.product_id=products.id")
-         .where("checkout_date IS NOT NULL AND checkout_date BETWEEN DATE(#{start_date}) AND DATE(#{end_date})")
-         .first.total
-    end 
-    week_total
+    Order.select("ROUND(SUM(quantity * products.price), 2) AS total, 
+                  ROUND((julianday(current_date) - julianday(checkout_date))/7, 0) AS wk")
+       .joins("JOIN order_contents ON order_contents.order_id=orders.id")
+       .joins("JOIN products ON order_contents.product_id=products.id")
+       .where("checkout_date IS NOT NULL AND checkout_date > ?", 49.days.ago)
+       .group("wk")
   end
 end
 
