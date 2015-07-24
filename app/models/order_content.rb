9f6b0ca670413fc01f17f4deaeea3fc917428af5
facingsouth <xin.d.liu@gmail.com>
2015-07-24 10:44:30 -0700

    user top states

--- app/models/order_content.rb
+++ app/models/order_content.rb
@@ -1,2 +1,17 @@
 class OrderContent < ActiveRecord::Base
+  belongs_to :order
+  belongs_to :product
+
+  def revenue(timeframe = nil)
+    if timeframe.nil?
+      OrderContent.select("ROUND(SUM(quantity * products.price), 2) AS total")
+                  .joins("JOIN products ON order_contents.product_id=products.id")
+                  .first.total
+    else
+      OrderContent.select("ROUND(SUM(quantity * products.price), 2) AS total")
+                  .joins("JOIN products ON order_contents.product_id=products.id")
+                  .where("order_contents.created_at > ?", timeframe.days.ago)
+                  .first.total
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  has_many :address
 
   # 1. Overall Platform
 
@@ -11,6 +12,15 @@ class User < ActiveRecord::Base
     else
       return User.where("created_at > ?", timeframe.days.ago).count
     end
+  end
+
+  def top_user_states(num=3)
+    User.select("COUNT(*) AS num_of_users, states.name")
+        .joins("JOIN addresses ON users.billing_id=addresses.id")
+        .joins("JOIN states ON addresses.state_id=states.id")
+        .group("states.name")
+        .order("num_of_users DESC")
+        .limit(num)
   end
 
 end
