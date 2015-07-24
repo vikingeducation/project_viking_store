a40e4ebfc471a5e5fcae6e145a5a9b9bb36768cd
ayva <olga.ayva@gmail.com>
2015-07-24 09:15:41 -0700

    building views

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ActiveRecord::Base
+  # has_many :addresses, :orders
+  # belongs_to :billing_id, , :class_name => "Address"
+  # belongs_to :shipping_id, , :class_name => "Address"
 end
