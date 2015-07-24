a0316bc4ed12e9e0972fdeea27beb2f49792a5d6
Garrett <garrettweinert@gmail.com>
2015-07-24 14:17:50 -0700

    hgjfg

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,7 @@
 class Order < ActiveRecord::Base
 
   def self.revenue
-    Order.find_by_sql("SELECT Round(SUM(price),2) as price from order_contents JOIN products ON products.id = product_id")
+    Order.find_by_sql("SELECT Round(SUM(quantity*price),2) as price from order_contents JOIN products ON products.id = product_id")
   end
 
   def self.count_last(num_days_ago)
