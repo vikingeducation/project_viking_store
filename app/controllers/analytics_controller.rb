acd89ab1a1d6aa230f1f114c13c023fdc95940f2
Nick Sarlo <nicholas.l.sarlo@gmail.com>
2015-07-24 18:55:29 -0500

    views started

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -1,5 +1,11 @@
 class AnalyticsController < ApplicationController
 
   def dashboard
+    @new_users_7 = User.user_count(7)
+    @orders_7 = Order.order_count(7)
+    @new_product_7 = Product.
+
+
+    @new_users_30 = User.user_count(30)
   end
 end
