86b66daa5f288536dc618f97ddb7c3f4e6f81a1a
alokp <alokpra@gmail.com>
2015-07-24 18:07:27 -0700

    working on avg vals

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,22 +3,22 @@ class DashboardController < ApplicationController
   	@user_seven = User.new_users(7)
   	@user_thirty = User.new_users(30)
   	@user_total = User.all.count
-  	@order_seven = Order.new_orders(7)
-  	@order_thirty = Order.new_orders(30)
+  	@order_seven = Order.new_orders(7.days.ago, 0.days.ago)
+  	@order_thirty = Order.new_orders(30.days.ago, 0.days.ago)
   	@order_total = Order.all.count
   	@product_seven = Product.new_products(7)
   	@product_thirty = Product.new_products(30)
   	@product_total = Product.all.count
-    @revenue_seven = Order.revenue(7)
-    @revenue_thirty = Order.revenue(30)
+    @revenue_seven = Order.revenue(7.days.ago, 0.days.ago)
+    @revenue_thirty = Order.revenue(30.days.ago, 0.days.ago)
     @revenue_total = Order.revenue
 
     @states = State.get_top_states
     @cities = City.get_top_cities
 
     @highest_single_order = Order.highest_single_order
-    @highest_7day_order = Order.highest_single_order(7)
-    @highest_30day_order = Order.highest_single_order(30)
+    @highest_7day_order = Order.highest_single_order(7.days.ago, 0.days.ago)
+    @highest_30day_order = Order.highest_single_order(30.days.ago, 0.days.ago)
 
     @lifetime_value = Order.lifetime_value
 
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,40 +1,61 @@
 class Order < ActiveRecord::Base
 
-def self.new_orders(input_day)
-	self.where("checkout_date > ?", input_day.days.ago).count
+def self.new_orders(start_day, end_day)
+	self.where("checkout_date > ? AND checkout_date < ?", start_day, end_day).count
 end
 
-def self.revenue_table(input)
+def self.revenue_table(start_day=99999999.days.ago, end_day=0.days.ago)
 	# if input is given:
-  table = self.where("checkout_date > ?", input.days.ago).joins("JOIN order_contents ON orders.id = order_contents.order_id").joins("JOIN products ON products.id = order_contents.product_id") if input
+  table = self.where("checkout_date > ? AND checkout_date < ?", start_day, end_day).joins("JOIN order_contents ON orders.id = order_contents.order_id").joins("JOIN products ON products.id = order_contents.product_id")
   # if no input, get all orders with checkout date
-  table = self.where("checkout_date IS NOT NULL").joins("JOIN order_contents ON orders.id = order_contents.order_id").joins("JOIN products ON products.id = order_contents.product_id") unless input
+  # table = self.where("checkout_date IS NOT NULL").joins("JOIN order_contents ON orders.id = order_contents.order_id").joins("JOIN products ON products.id = order_contents.product_id") unless input
   return table.select(:order_id, :quantity, :product_id, :price)
 end
 
-def self.revenue(input=nil)
-	table = revenue_table(input)
+def self.revenue(start_day=99999999.days.ago, end_day=0.days.ago)
+	table = revenue_table(start_day, end_day)
 	revenue = table.select("round(SUM(quantity * price), 2) AS sum")
 	revenue.first[:sum]
 end
 
-def self.highest_single_order(input=nil)
-  table = revenue_table(input)
+def self.highest_single_order(start_day=99999999.days.ago, end_day=0.days.ago)
+  table = revenue_table(start_day, end_day)
   revenue_table = table.select("round(SUM(quantity * price), 2) AS sum").group(:order_id).order("sum desc")
   users_revenue = revenue_table.joins("JOIN users ON user_id = users.id").select("users.first_name, users.last_name")
   [revenue_table.first[:sum], "#{users_revenue.first[:first_name]} #{users_revenue.first[:last_name]}"]
 end
 
-def self.lifetime_value(input=nil)
-  table = revenue_table(input)
+def self.lifetime_value(start_day=99999999.days.ago, end_day=0.days.ago)
+  table = revenue_table(start_day, end_day)
   revenue_table = table.select("round(SUM(quantity * price), 2) AS sum").group(:user_id).order("sum desc")
   users_revenue = revenue_table.joins("JOIN users ON user_id = users.id").select("users.first_name, users.last_name")
   [revenue_table.first[:sum], "#{users_revenue.first[:first_name]} #{users_revenue.first[:last_name]}"]
 end
 
+# def self.avg_value(input=99999999)
+
+#   table = Order.find_by_sql(["  SELECT first_name, last_name, AVG(total) AS average
+#                                 FROM(
+#                                 SELECT users.first_name, users.last_name, orders.id, product_id, SUM(quantity * price) AS total, user_id FROM orders
+#                                 JOIN order_contents ON orders.id = order_contents.order_id
+#                                 JOIN products ON products.id = order_contents.product_id
+#                                 JOIN users ON user_id = users.id
+#                                 WHERE orders.checkout_date > ?
+#                                 GROUP BY orders.id
+#                                 ORDER BY user_id
+#                                 )
+#                                 GROUP BY user_id
+#                                 ORDER BY average desc
+#                                 LIMIT 1", input.days.ago])
+
+#   return [table[0][:average], "#{table[0][:first_name]} #{table[0][:last_name]}"]
+
+# end
+
+
 def self.avg_value(input=99999999)
 
-  table = Order.find_by_sql(["   SELECT first_name, last_name, AVG(total) AS average FROM(
+  table = Order.find_by_sql(["
                                 SELECT users.first_name, users.last_name, orders.id, product_id, SUM(quantity * price) AS total, user_id FROM orders
                                 JOIN order_contents ON orders.id = order_contents.order_id
                                 JOIN products ON products.id = order_contents.product_id
@@ -42,10 +63,10 @@ def self.avg_value(input=99999999)
                                 WHERE orders.checkout_date > ?
                                 GROUP BY orders.id
                                 ORDER BY user_id
-                                )
-                                GROUP BY user_id
-                                ORDER BY average desc
-                                LIMIT 1", input.days.ago])
+                                ", input.days.ago])
+
+  total = table.inject(0) {|sum, val| sum + val[:total]}
+  avg = total/table.length
 
   return [table[0][:average], "#{table[0][:first_name]} #{table[0][:last_name]}"]
 
