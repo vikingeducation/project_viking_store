60ef6b9b77d6de81b5d15324ce051eee47def231
shadefinale <shadefinale@gmail.com>
2015-07-25 17:16:24 -0500

    Categories completed.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -7,16 +7,45 @@ class CategoriesController < ApplicationController
     @category = Category.new
   end
 
+  def show
+    @category = Category.find(params[:id])
+  end
+
   def create
     @category = Category.new(whitelist_category_params)
     if @category.save
-      flash[:success] = 'Category #{@category.name} successfully created!'
-      redirect @category
+      flash[:success] = "Category #{@category.name} successfully created!"
+      redirect_to @category
     else
-      flash[:error] = 'Oops, there was an error creating your category'
-       render :new
-     end
+      flash.now[:danger] = 'Oops, there was an error creating your category'
+      render :new
+    end
+  end
 
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(whitelist_category_params)
+      flash[:success] = "Category #{@category.name} successfully updated!"
+      redirect_to @category
+    else
+      flash.now[:danger] = 'Oops, there was an error updating your category'
+      render :edit
+    end
+  end
+
+  def destroy
+    @category = Category.find(params[:id])
+    if @category.destroy
+      flash[:success] = "Category #{@category.name} successfully deleted!"
+      redirect_to categories_path
+    else
+      flash.now[:danger] = 'Oops, there was an error deleting your category'
+      render @category
+    end
   end
 
   private
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,8 @@
 class Category < ActiveRecord::Base
-  validates  :name, :uniqueness => true , :presence => true
+  validates  :name, {:uniqueness => true , :presence => true}
   validates  :description,  :presence => true
-  
+
+  def products
+    Product.where(category_id: self.id)
+  end
 end
