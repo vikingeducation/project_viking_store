0ee3214c6011e4b7302fbe53803ca1d6b0940d41
mikebutts <mgbutts@gmail.com>
2015-07-25 03:29:21 -0400

    fixed legibility

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,6 +19,11 @@ class User < ActiveRecord::Base
     User.find_by_sql("SELECT users.first_name, users.last_name, ROUND(avg(price*quantity),2) AS sum, quantity FROM products JOIN order_contents ON products.id=order_contents.product_id JOIN orders ON orders.id = order_contents.order_id JOIN users ON orders.user_id = users.id GROUP BY user_id ORDER BY sum DESC LIMIT 1")
   end
   def self.most_orders_placed
-    User.find_by_sql("SELECT users.first_name, users.last_name, count(order_id) AS sum FROM products JOIN order_contents ON products.id=order_contents.product_id JOIN orders ON orders.id = order_contents.order_id JOIN users ON orders.user_id = users.id GROUP BY order_id ORDER BY sum DESC LIMIT 1")
+    User.find_by_sql("SELECT users.first_name, users.last_name, count(order_id) AS sum 
+      FROM products 
+      JOIN order_contents ON products.id=order_contents.product_id
+      JOIN orders ON orders.id = order_contents.order_id 
+      JOIN users ON orders.user_id = users.id 
+      GROUP BY order_id ORDER BY sum DESC LIMIT 1")
   end
 end
