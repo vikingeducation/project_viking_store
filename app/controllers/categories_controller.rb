4dafd16137d72e535107f2591f07423bbad3af70
mikebutts <mgbutts@gmail.com>
2015-07-25 17:46:27 -0400

    styling and new page created

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,4 +2,25 @@ class CategoriesController < ApplicationController
   def index
     @categories = Category.all
   end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(whitelist_category_params)
+    if @category.save
+      flash[:success] = 'Category #{@category.name} successfully created!'
+      redirect @category
+    else
+      flash[:error] = 'Oops, there was an error creating your category'
+       render :new
+     end
+
+  end
+
+  private
+    def whitelist_category_params
+      params.require(:category).permit(:name, :description)
+    end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,5 @@
 class Category < ActiveRecord::Base
+  validates  :name, :uniqueness => true , :presence => true
+  validates  :description,  :presence => true
+  
 end
