bf1ba3e2faeb12fc3ff5d0e809bcbc546cda59cc
Alice Fung <alice.o.fung@gmail.com>
2015-07-24 21:01:05 -0400

    changes during internet failure

--- app/models/order.rb
+++ app/models/order.rb
@@ -25,6 +25,35 @@ class Order < ActiveRecord::Base
 
   end
 
+  def orders_by_day #shows last 7 days D, Q, Value
+    now = Time.now
+    result = {}
+    7.times do |i|
+      day = now + i
+      result[day] = time_order_summary(day)
+    end
+  end
+
+  def time_order_summary(date, day_span = 0)
+    order_join_table = Order.joins("JOIN order_contents ON orders.id = order_id").joins("JOIN products ON products.id = product_id")
+
+    value = order_join_table.select("*") \
+          .where("checkout_date BETWEEN ? AND ?", date.midnight, date.end_of_day).sum("price * quantity")
+
+    quantity = order_join_table.where("checkout_date BETWEEN ? AND ?", day_span.days.ago.midnight, date.end_of_day).count("DISTINCT order_id")
+
+    [quantity, value]
+  end
+
+  def orders_by_week
+    now = Time.now
+    result = {}
+    7.times do |i|
+      start_weekday = now + (i+1) *7
+      result[start_weekday] = time_order_summary(start_weekday, 7)
+    end
+  end
+
 
 
 
