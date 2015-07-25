d53af79ce473f7518fe2564e54505540cfb4ad0b
shadefinale <shadefinale@gmail.com>
2015-07-25 16:17:35 -0500

    working on categories.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'dashboard#index'
-    
+
   get 'dashboard/admin' => 'dashboard#admin'
   resources :dashboard, only: :index
-  
+  resources :categories
 end
