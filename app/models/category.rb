55d7a2409e2d36242050f071b4cb1d0e6f193d47
shadefinale <shadefinale@gmail.com>
2015-07-25 19:58:59 -0500

    Final commit (for now)

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ActiveRecord::Base
-  validates  :name, :sku, {:uniqueness => true , :presence => true}
-  validates  :description, :price, :presence => true
+  validates  :name, {:uniqueness => true , :presence => true}
+  validates  :description, :presence => true
 
   def products
     Product.where(category_id: self.id)
