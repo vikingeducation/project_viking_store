63f97a52dafa5c5e099aad38690c7470765aecec
shadefinale <shadefinale@gmail.com>
2015-07-27 17:35:29 -0500

    Finished basic product information and RESTful routes.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,56 @@
 class ProductsController < ApplicationController
-
-
   def index
     @products = Product.all
   end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(whitelist_product_params)
+    if @product.save
+      flash[:success] = "Your Product #{@product.name} has been created!"
+      redirect_to @product
+    else
+      flash[:danger] = "There was an error creating your product, try again."
+      render :new
+    end
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(whitelist_product_params)
+      flash[:success] = "Your Product #{@product.name} has been edited!"
+      redirect_to @product
+    else
+      flash[:danger] = "There was an error editing your product, try again."
+      render :edit
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    if @product.destroy
+      flash[:success] = "Your Product #{@product.name} has been deleted!"
+      redirect_to products_path
+    else
+      flash[:danger] = "There was an error deleting your product, try again."
+      redirect_to @product
+    end
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  private
+    def whitelist_product_params
+      params.require(:product).permit(:name, :sku, :price, :description, :category_id)
+    end
 
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,6 @@
 class Product < ActiveRecord::Base
-
   has_many :order_contents
-  belongs_to :categories
+  belongs_to :category
   has_many :orders, through: :order_contents
 
   def self.product_count(timeframe = 1000000)
@@ -17,5 +16,17 @@ class Product < ActiveRecord::Base
   def self.delete_category(cat_id)
     Product.where("category_id = ?", cat_id)
             .update_all(:category_id => nil)
+  end
+
+  def times_ordered
+    Product.joins("JOIN order_contents ON products.id = order_contents.product_id").
+    joins("JOIN orders ON orders.id = order_contents.order_id").
+    where("product_id = #{self.id} AND orders.checkout_date IS NOT NULL").count
+  end
+
+  def carts_in
+    Product.joins("JOIN order_contents ON products.id = order_contents.product_id").
+    joins("JOIN orders ON orders.id = order_contents.order_id").
+    where("product_id = #{self.id} AND orders.checkout_date IS NULL").count
   end
 end
