f1cfa990287f54942955e5d7b88bdfc2219b5d8d
shadefinale <shadefinale@gmail.com>
2015-07-28 11:34:30 -0500

    Working on new address form.

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,13 +1,32 @@
 class AddressesController < ApplicationController
   def index
-    
-    @addresses = if params[:user_id]
-      @addresses = Address.where("user_id = #{params[:user_id]}")
-    else
-      Address.all
-    end
+    @user = User.find_by(id: params[:user_id])
+    @addresses = params[:user_id] ? render_user(params[:user_id]) : render_all
+  end
+
+  def new
+    @address = Address.new
+    @address.user = User.find(params[:user_id])
+  end
+
+  def create
   end
 
   def show
   end
+
+  private
+    def render_all
+      Address.all
+    end
+
+    def render_user(user_id)
+      addresses = Address.where("user_id = #{user_id}")
+      if addresses.empty?
+        flash[:notice] = "User not found."
+        return Address.all
+      else
+        return addresses
+      end
+    end
 end
