18f102678ef1c6ec453abc406857a2093a255d7f
shadefinale <shadefinale@gmail.com>
2015-07-31 11:59:12 -0500

    Namespace addresses under admin.

--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,10 @@ Rails.application.routes.draw do
   resources :categories
   resources :products
   resources :users
-  resources :addresses
   resources :orders
+  namespace :admin do
+    resources :addresses
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
