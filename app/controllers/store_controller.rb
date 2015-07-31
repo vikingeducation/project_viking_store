b8e0056dd5086d0e55418b20233cbdeab741b7bf
mikebutts <mgbutts@gmail.com>
2015-07-31 14:59:44 -0400

    poruct index page

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,6 +1,13 @@
 class StoreController < ApplicationController
 
   def home
-
+    @categories = Category.all  
+    
+    if params[:cat_id]
+      @products = Category.find(params[:cat_id]).products
+    else
+      @products = Product.all
+    end
+    
   end
 end
