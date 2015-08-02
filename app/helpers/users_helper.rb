024b4f813942453b1418935d6a5b9d3a9695f5ec
Adam <adam.kinson@gmail.com>
2015-08-02 15:10:52 -0400

    fix New/Edit User text around submit button

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,20 @@
 module UsersHelper
+
+  def edit_button_title
+    if signed_in_user?
+      "Update User Info"
+    else
+      "Complete Sign Up"
+    end
+  end
+
+
+  def edit_button_text
+    if signed_in_user?
+      "Submit Changes"
+    else
+      "Sign Up"
+    end
+  end
+
 end
