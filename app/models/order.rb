3da93cf14848f159db6f809f438fd8b268384c48
Chris Scavello <bideowego@gmail.com>
2015-09-26 04:03:43 -0400

    Complete dashboard skeleton, all time totals

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,34 @@
 class Order < ActiveRecord::Base
+  has_many :items, :class_name => 'OrderContent'
   belongs_to :user
   belongs_to :shipping, :class_name => 'Address'
   belongs_to :billing, :class_name => 'Address'
   belongs_to :credit_card
+
+  # Returns the revenue for this order
+  def revenue
+    sql = 'SUM(order_contents.quantity * products.price) AS revenue'
+    OrderContent.select(sql)
+      .joins(:product)
+      .joins(:order)
+      .where('order_contents.order_id = ?', id)
+      .limit(1)
+      .to_a
+      .first
+      .revenue
+      .to_f
+  end
+
+  # Returns the total revenue for all orders
+  def self.revenue
+    sql = 'SUM(order_contents.quantity * products.price) AS revenue'
+    OrderContent.select(sql)
+      .joins(:product)
+      .joins(:order)
+      .limit(1)
+      .to_a
+      .first
+      .revenue
+      .to_f
+  end
 end
