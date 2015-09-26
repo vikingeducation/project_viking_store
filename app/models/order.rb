82fefcae770ef36635a75a47486bad68f9263cb1
Chris Scavello <bideowego@gmail.com>
2015-09-26 02:05:14 -0400

    Add model relations

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,6 @@
 class Order < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :shipping, :class_name => 'Address'
+  belongs_to :billing, :class_name => 'Address'
+  belongs_to :credit_card
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ActiveRecord::Base
+  has_one :billing, :class_name => 'Address'
+  has_one :shipping, :class_name => 'Address'
+  has_many :orders
+  has_many :addresses
+  has_many :credit_cards
 end
