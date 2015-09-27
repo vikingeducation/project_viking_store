7ac03c6147b5d50e0d9a9f9ff3c9f0da2c6cfa6a
Chris Scavello <bideowego@gmail.com>
2015-09-27 08:03:19 -0400

    Complete admin portal layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root :to => 'admin#analytics'
+  root :to => 'admin#index'
+  get '/analytics', :to => 'admin#analytics'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
