726a66730bed17f752d8b313a998c39fa6aaf07e
Justin Mullis <justin.mullis@gmail.com>
2015-10-22 21:27:19 -0400

    overall platform total

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,31 @@
 class DashboardController < ApplicationController
 
   def home
-    
+    @panels = []
+    @panels << build_overall
+    @panels
+  end
+
+
+  private
+
+
+  def build_overall
+    overall = {}
+    overall[:title] = "Overall Platform"
+    overall[:tables] = []
+
+    total_table = {}
+    total_table[:title] = "Total"
+    total_table[:rows] = []
+    total_table[:rows] << ["Users", User.count]
+    total_table[:rows] << ["Orders", Order.submitted_count]
+    total_table[:rows] << ["Products", Product.count]
+    total_table[:rows] << ["Revenue", Order.total_revenue]
+
+    overall[:tables] << total_table
+
+    overall
   end
 
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,23 @@
 class Order < ActiveRecord::Base
+
+  def self.submitted_count
+    Order.where("checkout_date IS NOT NULL").count
+  end
+
+  def self.total_revenue
+    Order.select("SUM(products.price) AS t").join_with_products.to_a.first.t
+  end
+
+  def self.join_with_products
+    join_with_order_contents.join_order_contents_with_products
+  end
+
+  def self.join_with_order_contents
+    joins("JOIN order_contents ON orders.id = order_contents.id ")
+  end
+
+  def self.join_order_contents_with_products
+    joins("JOIN products ON order_contents.product_id = products.id")
+  end
+
 end
