c11042bb20a8c1cbcd4d4f810210bf0d5b4fa8f4
Justin Mullis <justin.mullis@gmail.com>
2015-10-31 18:02:33 -0400

    nav_link helper

---
+++ app/helpers/admin_helper.rb
@@ -0,0 +1,21 @@
+module  AdminHelper
+  def nav_link(link_text, link_path, options = {})
+
+    # Determine if link is to active page (sets gray background)
+    class_name = current_page?(link_path) ? 'active' : nil
+
+    # Add a badge to the link, and extra classes to the li
+    if options[:badge]
+      badge = " <span class='badge'>#{options[:badge]}</span>"
+      link_text += " #{badge}"
+    elsif options[:class]
+      class_name = "#{class_name} #{options[:class]}"
+    end  
+     
+    # Generate  link  
+    link_to(link_path, class: class_name) do
+      link_text.html_safe
+    end
+
+  end
+end
