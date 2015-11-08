0559ff1afd794e2fcaeeae010df46e4541ba9737
Justin Mullis <justin.mullis@gmail.com>
2015-11-08 18:48:43 -0500

    address new and edit

--- app/controllers/admin/addresses_controller.rb
+++ app/controllers/admin/addresses_controller.rb
@@ -1,14 +1,74 @@
 class Admin::AddressesController < AdminController
 
   def index
-    if params[:user_id] && User.exists?(params[:user_id])
-      @user = User.find(params[:user_id])
+    if @user = valid_user
       @addresses = @user.addresses
-    elsif params[:user_id]
+    elsif params[:user_id] # user_id there but invalid
       flash[:danger] = "Invalid User!"
       @addresses = Address.all
     else
       @addresses = Address.all
+    end
+  end
+
+
+  def show
+    @address = Address.find(params[:id])
+  end
+
+
+  def new
+    if user = valid_user
+      @address = user.addresses.new
+      @address.build_city
+    else
+      flash[:danger] = "Address cannot be created without valid User!"
+      redirect_to admin_addresses_path
+    end
+  end
+
+
+  def create
+    @address = Address.new(address_params)
+    if @address.save
+      flash[:success] = "New Address for #{@address.user.first_name} Created!"
+      redirect_to admin_address_path(@address)
+    else
+      flash.now[:danger] = "Oops, something went wrong!"
+      render :new
+    end
+  end
+
+
+  def edit
+    @address = Address.find(params[:id])
+  end
+
+
+  def update
+    @address = Address.find(params[:id])
+    if @address.update(address_params)
+      flash[:info] = "Address for #{@address.user.first_name} Updated!"
+      redirect_to admin_address_path(@address)
+    else
+      flash.now[:danger] = "Oops, something went wrong!"
+      render :edit
+    end
+  end
+
+
+  private
+
+
+  def address_params
+    params.require(:address).permit(:user_id, :street_address, :state_id, 
+                                    :zip_code, city_attributes: [:name])
+  end
+
+
+  def valid_user
+    if params.has_key?(:user_id) && User.exists?(params[:user_id])
+      User.find(params[:user_id])
     end
   end
 
--- app/models/address.rb
+++ app/models/address.rb
@@ -2,13 +2,32 @@ class Address < ActiveRecord::Base
   belongs_to :user
   belongs_to :city
   belongs_to :state
+  accepts_nested_attributes_for :city
   has_many :billed_orders, foreign_key: :billing_id, class_name: "Order"
   has_many :shipped_orders, foreign_key: :shipping_id, class_name: "Order"
+
+  validates_associated :city
+  validates :street_address, :zip_code, length: { in: 1..64 }
+  validate :validate_state_id
+  validate :user_id
 
   def orders_count
     Order.select("COUNT(*) as num_orders").
     where( "shipping_id = :ship OR billing_id = :bill", { ship: id, bill: id } ).
     to_a.first.num_orders
+  end
+
+
+  private
+
+
+  def validate_state_id
+    errors.add(:state_id, "is invalid") unless State.exists?(self.state_id)
+  end
+
+
+  def validate_user_id
+    errors.add(:user_id, "is invalid") unless User.exists?(self.user_id)
   end
 
 end
