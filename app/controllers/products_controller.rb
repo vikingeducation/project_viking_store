84f79aa7f5dfeacc95854a09e1316cc3c3f4aee8
Justin Mullis <justin.mullis@gmail.com>
2015-11-22 12:34:25 -0500

    filter by category

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,14 @@
 class ProductsController < ApplicationController
 
   def index
-    @products = Product.all.order(:name)    
+    @categories = Category.joins(:products).distinct
+
+    if @category = Category.find_by_id(params[:category])
+      @products = @category.products
+    else
+      @products = Product.all.order(:name)
+    end
+   
   end
 
 end
