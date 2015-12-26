f766ba1d3e113a9b8d2fe32f8e2f135e4f489c35
Sia <siakaramalegos@gmail.com>
2015-12-26 14:46:54 -0600

    Adds first two behaviors and a scope for completed orders

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -8,6 +8,12 @@ class DashboardController < ApplicationController
     # Panel 2: User Demographics and Behavior
     @top_states = User.select("s.name, COUNT(*) as count").joins(user_state_join).group("s.id").order("count DESC").limit(3)
     @top_cities = User.select("c.name, COUNT(*) as count").joins(user_city_join).group("c.id").order("count DESC").limit(3)
+    @behavior_stats = [
+      {criteria: 'Highest Single Order Value', result: get_highest_order_user, currency: true},
+      {criteria: 'Highest Lifetime Value', result: get_highest_lifetime_user, currency: true}#,
+      # {criteria: 'Highest Average Order Value', result: get_highest_avg_order_user, currency: true},
+      # {criteria: 'Most Orders Placed' result: get_most_orders_user, currency: false}
+    ]
   end
 
   private
@@ -33,11 +39,27 @@ class DashboardController < ApplicationController
   def get_revenue(days_ago = nil)
     select_revenue = "SUM(p.price * oc.quantity) as revenue"
     if days_ago
-      relation = Order.select(select_revenue).joins(order_product_join).days_ago(days_ago)
+      relation = Order.select(select_revenue).joins(order_product_join).days_ago(days_ago).completed
     else
-      relation = Order.select(select_revenue).joins(order_product_join)
+      relation = Order.select(select_revenue).joins(order_product_join).completed
     end
     relation[0].revenue
+  end
+
+  def get_highest_order_user
+    User.select("users.first_name, users.last_name, MAX(p.price * oc.quantity) as amount").joins(user_products_join).group("o.id, users.id").where(order_completed).order("amount DESC").limit(1)[0]
+  end
+
+  def get_highest_lifetime_user
+    User.select("users.first_name, users.last_name, SUM(p.price * oc.quantity) as amount").joins(user_products_join).where(order_completed).group("users.id").order("amount DESC").limit(1)[0]
+  end
+
+  def order_completed
+    "o.checkout_date IS NOT NULL"
+  end
+
+  def user_products_join
+    "JOIN orders o ON o.user_id = users.id JOIN order_contents oc ON oc.order_id = o.id JOIN products p ON p.id = oc.product_id"
   end
 
   def order_product_join
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,5 @@
 class Order < ActiveRecord::Base
   scope :days_ago, -> (days_past = 7) { where("checkout_date > ?", days_past.days.ago) }
+
+  scope :completed, -> { where("checkout_date IS NOT NULL")}
 end
