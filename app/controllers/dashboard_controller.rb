16227a3ea2b340130f876d622d872329072c8266
Sia <siakaramalegos@gmail.com>
2015-12-26 18:17:16 -0600

    Refactors around order_totals table and completes panel 3

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -9,14 +9,24 @@ class DashboardController < ApplicationController
     @top_states = User.select("s.name, COUNT(*) as count").joins(user_state_join).group("s.id").order("count DESC").limit(3)
     @top_cities = User.select("c.name, COUNT(*) as count").joins(user_city_join).group("c.id").order("count DESC").limit(3)
     @behavior_stats = [
-      {criteria: 'Highest Single Order Value', result: get_highest_order_user, currency: true},
-      {criteria: 'Highest Lifetime Value', result: get_highest_lifetime_user, currency: true},
-      {criteria: 'Highest Average Order Value', result: get_highest_avg_order_user, currency: true},
-      {criteria: 'Most Orders Placed', result: get_most_orders_user, currency: false}
+      {criteria: 'Highest Single Order Value',
+        result: get_highest_order_user,
+        currency: true},
+      {criteria: 'Highest Lifetime Value',
+        result: get_highest_aggregation_user('SUM'),
+        currency: true},
+      {criteria: 'Highest Average Order Value',
+        result: get_highest_aggregation_user('AVG'),
+        currency: true},
+      {criteria: 'Most Orders Placed',
+        result: get_highest_aggregation_user('COUNT'),
+        currency: false}
     ]
 
     # Panel 3: Order Statistics
-    # @order_stats_7_days = get_order_stats(7)
+    @order_stats_7_days = get_order_stats(7)
+    @order_stats_30_days = get_order_stats(30)
+    @order_stats_total = get_order_stats
   end
 
   private
@@ -40,19 +50,21 @@ class DashboardController < ApplicationController
   end
 
   def get_order_stats(days_ago = nil)
-    # if days_ago
-    #   {
-    #     num_orders: Order.days_ago(days_ago).completed.count,
-    #     total_revenue: get_revenue(days_ago),
-    #     avg_order_value: get_avg_order(days_ago)
-    #   }
-    # else
-    #   {
-    #     num_orders: Order.completed.count,
-    #     total_revenue: get_revenue(days_ago),
-    #     avg_order_value: get_avg_order
-    #   }
-    # end
+    if days_ago
+      {
+        num_orders: Order.days_ago(days_ago).completed.count,
+        total_revenue: get_revenue(days_ago),
+        avg_order_value: get_aggregation_order('AVG', days_ago),
+        max_order_value: get_aggregation_order('MAX', days_ago)
+      }
+    else
+      {
+        num_orders: Order.completed.count,
+        total_revenue: get_revenue(days_ago),
+        avg_order_value: get_aggregation_order('AVG'),
+        max_order_value: get_aggregation_order('MAX')
+      }
+    end
   end
 
   def get_revenue(days_ago = nil)
@@ -65,52 +77,48 @@ class DashboardController < ApplicationController
     relation[0].revenue
   end
 
-  def get_avg_order(days_ago = nil)
-    select_revenue = "SUM(p.price * oc.quantity) as revenue"
+  def get_aggregation_order(aggregator, days_ago = nil)
+    query = aggregate_order_query(aggregator)
+    Order.find_by_sql(query)[0].amount
+  end
+
+  def aggregate_order_query(aggregator, days_ago = nil)
+    "SELECT #{aggregator}(revenue) as amount
+    FROM (#{order_totals_query(days_ago)}) ot"
+  end
+
+  # Returns order_id as id, user_id, revenue
+  def order_totals_query(days_ago = nil)
     if days_ago
-      relation = Order.select(select_revenue).joins(order_product_join).days_ago(days_ago).completed.group("order.id")
+      where_clause = "< '#{days_ago.days.ago}'"
     else
-      relation = Order.select(select_revenue).joins(order_product_join).completed.group("order.id")
+      where_clause = "IS NOT NULL"
     end
-    relation[0].revenue
+
+    "SELECT o.id, o.user_id, SUM(p.price * oc.quantity) as revenue
+      FROM orders o
+        JOIN order_contents oc ON oc.order_id = o.id
+        JOIN products p ON p.id = oc.product_id
+      WHERE o.checkout_date #{where_clause}
+      GROUP BY o.id"
+  end
+
+  # Join clause to join users with order_totals table
+  def user_order_totals_join(days_ago = nil)
+    "JOIN (#{order_totals_query(days_ago)}) ot ON ot.user_id = users.id"
+  end
+
+  # Returns first_name, last_name, revenue for each order
+  def user_order_totals(days_ago = nil)
+    User.select("users.first_name, users.last_name, ot.revenue as amount").joins(user_order_totals_join)
   end
 
   def get_highest_order_user
-    User.select("users.first_name, users.last_name, SUM(p.price * oc.quantity) as amount").joins(user_products_join).group("o.id, users.id").where(order_completed).order("amount DESC").limit(1)[0]
+    user_order_totals.order("amount DESC").limit(1)[0]
   end
 
-  def get_highest_lifetime_user
-    User.select("users.first_name, users.last_name, SUM(p.price * oc.quantity) as amount").joins(user_products_join).where(order_completed).group("users.id").order("amount DESC").limit(1)[0]
-  end
-
-  def get_highest_avg_order_user
-    query = "
-      SELECT first_name, last_name, AVG(order_total) as amount
-      FROM (
-        SELECT o.user_id, u.first_name, u.last_name, SUM(p.price * oc.quantity) as order_total
-        FROM users u
-          JOIN orders o ON o.user_id = u.id
-          JOIN order_contents oc ON oc.order_id = o.id
-          JOIN products p ON p.id = oc.product_id
-        WHERE o.checkout_date IS NOT NULL
-        GROUP BY o.id, u.id) as totals
-      GROUP BY user_id, first_name, last_name
-      ORDER BY amount DESC
-      LIMIT 1"
-
-    User.find_by_sql(query)[0]
-  end
-
-  def get_most_orders_user
-    User.select("users.first_name, users.last_name, COUNT(o.id) as amount").joins("JOIN orders o on o.user_id = users.id").group("users.id").where(order_completed).order("amount DESC").limit(1)[0]
-  end
-
-  def order_completed
-    "o.checkout_date IS NOT NULL"
-  end
-
-  def user_products_join
-    "JOIN orders o ON o.user_id = users.id JOIN order_contents oc ON oc.order_id = o.id JOIN products p ON p.id = oc.product_id"
+  def get_highest_aggregation_user(aggregator)
+    User.select("users.first_name, users.last_name, #{aggregator}(ot.revenue) as amount").joins(user_order_totals_join).group("users.id").order("amount DESC").limit(1)[0]
   end
 
   def order_product_join
