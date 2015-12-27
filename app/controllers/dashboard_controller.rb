98d4118cbcbb1a6a47a7209347b00a143fb2d691
Sia <siakaramalegos@gmail.com>
2015-12-27 15:00:08 -0600

    Refactors to put user queries into user model

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -8,20 +8,7 @@ class DashboardController < ApplicationController
     # Panel 2: User Demographics and Behavior
     @top_states = State.top_3_by_users
     @top_cities = City.top_3_by_users
-    @behavior_stats = [
-      {criteria: 'Highest Single Order Value',
-        result: get_highest_order_user,
-        currency: true},
-      {criteria: 'Highest Lifetime Value',
-        result: get_highest_aggregation_user('SUM'),
-        currency: true},
-      {criteria: 'Highest Average Order Value',
-        result: get_highest_aggregation_user('AVG'),
-        currency: true},
-      {criteria: 'Most Orders Placed',
-        result: get_highest_aggregation_user('COUNT'),
-        currency: false}
-    ]
+    @behavior_stats = get_behavior_stats
 
     # Panel 3: Order Statistics
     @order_stats_7_days = Order.get_order_stats(7)
@@ -53,22 +40,21 @@ class DashboardController < ApplicationController
     end
   end
 
-  # Join clause to join users with order_totals table
-  def user_order_totals_join(days_ago = nil)
-    "JOIN (#{Order.order_totals_query(days_ago)}) ot ON ot.user_id = users.id"
-  end
-
-  # Returns first_name, last_name, revenue for each order
-  def user_order_totals(days_ago = nil)
-    User.select("users.first_name, users.last_name, ot.revenue as amount").joins(user_order_totals_join)
-  end
-
-  def get_highest_order_user
-    user_order_totals.order("amount DESC").limit(1)[0]
-  end
-
-  def get_highest_aggregation_user(aggregator)
-    User.select("users.first_name, users.last_name, #{aggregator}(ot.revenue) as amount").joins(user_order_totals_join).group("users.id").order("amount DESC").limit(1)[0]
+  def get_behavior_stats
+    [
+      {criteria: 'Highest Single Order Value',
+        result: User.get_highest_order_user,
+        currency: true},
+      {criteria: 'Highest Lifetime Value',
+        result: User.get_highest_aggregation_user('SUM'),
+        currency: true},
+      {criteria: 'Highest Average Order Value',
+        result: User.get_highest_aggregation_user('AVG'),
+        currency: true},
+      {criteria: 'Most Orders Placed',
+        result: User.get_highest_aggregation_user('COUNT'),
+        currency: false}
+    ]
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,24 @@
 class User < ActiveRecord::Base
   scope :days_ago, -> (days_past = 7) { where("created_at > ?", days_past.days.ago) }
+
+  private
+
+  # Join clause to join users with order_totals table
+  def self.user_order_totals_join(days_ago = nil)
+    "JOIN (#{Order.order_totals_query(days_ago)}) ot ON ot.user_id = users.id"
+  end
+
+  # Returns first_name, last_name, revenue for each order
+  def self.user_order_totals(days_ago = nil)
+    User.select("users.first_name, users.last_name, ot.revenue as amount").joins(user_order_totals_join)
+  end
+
+  def self.get_highest_order_user
+    user_order_totals.order("amount DESC").limit(1)[0]
+  end
+
+  def self.get_highest_aggregation_user(aggregator)
+    User.select("users.first_name, users.last_name, #{aggregator}(ot.revenue) as amount").joins(user_order_totals_join).group("users.id").order("amount DESC").limit(1)[0]
+  end
+
 end
