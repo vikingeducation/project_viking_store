96caaf10261abc637c7eb17aa812979fccffe552
Sia <siakaramalegos@gmail.com>
2015-12-30 12:37:47 -0600

    Adds new and create for categories

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,6 +1,6 @@
 class Admin::CategoriesController < ApplicationController
   layout 'admin'
-  before_action :set_category
+  before_action :set_category, only: [:show, :edit, :update, :delete]
 
   def index
     @categories = Category.all
@@ -9,10 +9,28 @@ class Admin::CategoriesController < ApplicationController
   def show
   end
 
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to admin_categories_url, notice: 'Category successfully created!'
+    else
+      render :new
+    end
+  end
+
   private
 
   def set_category
     @category = Category.find(params[:id])
     @products = Product.where(category_id: params[:id])
+  end
+
+  def category_params
+    params.require(:category).permit(:name)
   end
 end
