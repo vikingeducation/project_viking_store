4f99f1da2808737227e6bfc6d7b1b1f25143bf4b
Sia <siakaramalegos@gmail.com>
2015-12-30 12:09:01 -0600

    Adds index and show for categories

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,7 +1,18 @@
 class Admin::CategoriesController < ApplicationController
   layout 'admin'
+  before_action :set_category
 
   def index
     @categories = Category.all
+  end
+
+  def show
+  end
+
+  private
+
+  def set_category
+    @category = Category.find(params[:id])
+    @products = Product.where(category_id: params[:id])
   end
 end
