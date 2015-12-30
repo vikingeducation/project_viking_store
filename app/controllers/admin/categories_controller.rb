359d89bff4192fd4b21a6cdb25ae50fa10e2e8b5
Sia <siakaramalegos@gmail.com>
2015-12-30 12:47:32 -0600

    Adds edit and delete for categories

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,6 +1,6 @@
 class Admin::CategoriesController < ApplicationController
   layout 'admin'
-  before_action :set_category, only: [:show, :edit, :update, :delete]
+  before_action :set_category, only: [:show, :edit, :update, :destroy]
 
   def index
     @categories = Category.all
@@ -20,6 +20,25 @@ class Admin::CategoriesController < ApplicationController
       redirect_to admin_categories_url, notice: 'Category successfully created!'
     else
       render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @category.update(category_params)
+      redirect_to admin_categories_url, notice: 'Category successfully updated!'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @category.destroy
+      redirect_to admin_categories_url, notice: 'Category successfully deleted.'
+    else
+      redirect_to admin_categories_url, alert: 'Unable to delete category.'
     end
   end
 
