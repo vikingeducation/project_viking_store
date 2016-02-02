307a9ad0438941c503bb6a0598169a7d5400ed35
Andrew Baik <and.baik@gmail.com>
2016-02-02 14:57:43 -0500

    finished panel 2, started panel 3

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,7 @@ class User < ActiveRecord::Base
 
 
   def self.highest_single_order
-    User.select("SUM(oc.quantity * p.price) AS order_value, 
+    User.select("SUM(oc.quantity * p.price) AS order_value,
       concat(u.first_name, ' ', u.last_name) AS user")
         .joins("AS u JOIN orders o ON o.user_id = u.id")
         .joins("JOIN order_contents oc ON o.id = oc.order_id")
@@ -28,7 +28,7 @@ class User < ActiveRecord::Base
 
 
   def self.highest_lifetime
-    User.select("SUM(oc.quantity * p.price) AS lifetime_total, 
+    User.select("SUM(oc.quantity * p.price) AS lifetime_total,
        concat(u.first_name, ' ', u.last_name) AS user")
         .joins("AS u JOIN orders o ON o.user_id = u.id")
         .joins("JOIN order_contents oc ON o.id = oc.order_id")
@@ -36,10 +36,27 @@ class User < ActiveRecord::Base
         .where("o.checkout_date IS NOT null")
         .group("u.id")
         .order("lifetime_total DESC")
-        .limit(1)   
+        .limit(1)
   end
 
+  def self.highest_average
+    User.select("ROUND(AVG(oc.quantity * p.price),2) AS avg_order_value, concat(u.first_name, ' ', u.last_name) AS user")
+      .joins("AS u JOIN orders o ON o.user_id=u.id")
+      .joins("JOIN order_contents oc ON o.id = oc.order_id")
+      .joins("JOIN products p ON p.id = oc.product_id")
+      .where("o.checkout_date IS NOT null")
+      .group("u.id")
+      .order("avg_order_value DESC")
+      .limit(1)
+  end
 
+  def self.most_ordered
+    User.select("COUNT(o.id) AS order_count, concat(u.first_name, ' ', u.last_name) AS user")
+      .joins("AS u JOIN orders o ON o.user_id=u.id")
+      .group("u.id")
+      .order("order_count DESC")
+      .limit(1)
+  end
 
 
 end
