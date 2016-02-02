2dc83f24158e522b288e771e4ef61f38f31b04a7
Kit Langton <kitlangton@gmail.com>
2016-02-02 14:58:40 -0500

    oka

--- app/models/order.rb
+++ app/models/order.rb
@@ -14,7 +14,28 @@ class Order < ActiveRecord::Base
   end
 
   def self.top_states
-    Order.processed.select("states.name").joins("JOIN addresses ON addresses.id = orders.billing_id JOIN states ON states.id = addresses.state_id").group("states.name").order("COUNT(states.name) DESC").limit(3).count
+    Order.processed.select("states.name").
+      joins("JOIN addresses ON addresses.id = orders.billing_id").
+      joins("JOIN states ON states.id = addresses.state_id").
+      group("states.name").
+      order("COUNT(states.name) DESC").
+      limit(3)
+      .count
   end
 
+  def self.average_value_since(num_days)
+    Order.find_by_sql("SELECT AVG(totals.order_total) FROM (#{order_totals.to_sql}) AS totals WHERE order_totals.created_at <= ?", num_days.days.ago).first.avg
+  end
+
+  def self.average_value
+    Order.find_by_sql("SELECT AVG(totals.order_total) FROM (#{order_totals.to_sql}) AS totals").first.avg
+  end
+
+  def self.largest_value
+    Order.find_by_sql("SELECT MAX(totals.order_total) FROM (#{order_totals.to_sql}) AS totals").first.max
+  end
+
+  def self.order_totals
+    Order.processed.select("orders.*, SUM(price * quantity) AS order_total").joins("JOIN order_contents ON order_id = orders.id JOIN products ON product_id = products.id").group("orders.id")
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -18,15 +18,15 @@ class User < ActiveRecord::Base
   end
 
   def self.highest_order_value
-    User.select("users.*, SUM(price * quantity)").joins("JOIN orders ON user_id = users.id JOIN order_contents ON order_id = orders.id JOIN products ON product_id = products.id").group("users.id, order_id").order("SUM(price * quantity) DESC")
+    User.select("users.*, SUM(price * quantity)").joins("JOIN orders ON user_id = users.id JOIN order_contents ON order_id = orders.id JOIN products ON product_id = products.id").where("checkout_date IS NOT NULL").group("users.id, order_id").order("SUM(price * quantity) DESC")
   end
 
   def self.highest_lifetime_order_value
-    User.select("users.*, SUM(price * quantity)").joins("JOIN orders ON user_id = users.id JOIN order_contents ON order_id = orders.id JOIN products ON product_id = products.id").group("users.id").order("SUM(price * quantity) DESC")
+    User.select("users.*, SUM(price * quantity)").joins("JOIN orders ON user_id = users.id JOIN order_contents ON order_id = orders.id JOIN products ON product_id = products.id").where("checkout_date IS NOT NULL").group("users.id").order("SUM(price * quantity) DESC")
   end
 
   def self.highest_average_order_value
-    subquery = User.select("users.*, SUM(price * quantity) AS order_sum").joins("JOIN orders ON user_id = users.id JOIN order_contents ON order_id = orders.id JOIN products ON product_id = products.id").group("users.id, order_id").order("SUM(price * quantity) DESC").to_sql
+    subquery = User.select("users.*, SUM(price * quantity) AS order_sum").joins("JOIN orders ON user_id = users.id JOIN order_contents ON order_id = orders.id JOIN products ON product_id = products.id").where("checkout_date IS NOT NULL").group("users.id, order_id").order("SUM(price * quantity) DESC").to_sql
 
      User.select("users.*, AVG(order_values.order_sum)").joins("JOIN (#{subquery}) AS order_values ON order_values.id = users.id").group("users.id").order("avg DESC")
   end
