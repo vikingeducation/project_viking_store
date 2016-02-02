bcbd0a440a72053f7d0bc3672c7592cc632fb0fc
Joshua Masland-Sarani <j.masland@gmail.com>
2016-02-02 13:33:45 -0500

    DRY things out

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,9 +4,7 @@ class Order < ActiveRecord::Base
   end
 
   def self.total_revenue_generated
-    Order.joins(
-      "JOIN order_contents ON orders.id = order_contents.order_id JOIN products ON order_contents.product_id = products.id"
-      ).where(
+    join_with_products.where(
       "orders.checkout_date IS NOT NULL"
       ).sum(
       "products.price * order_contents.quantity"
@@ -18,12 +16,16 @@ class Order < ActiveRecord::Base
   end
 
   def self.revenue_30
-    Order.joins(
-      "JOIN order_contents ON orders.id = order_contents.order_id JOIN products ON order_contents.product_id = products.id"
-      ).where(
+    join_with_products.where(
       "orders.created_at > ( CURRENT_DATE - 30 ) AND orders.checkout_date IS NOT NULL"
       ).sum(
       "products.price * order_contents.quantity"
+      )
+  end
+
+  def self.join_with_products
+    Order.joins(
+      "JOIN order_contents ON orders.id = order_contents.order_id JOIN products ON order_contents.product_id = products.id"
       )
   end
 
