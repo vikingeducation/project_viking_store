efce5c3013ca8364454bb80c0f0ddfda46b0cad8
Joshua Masland-Sarani <j.masland@gmail.com>
2016-02-03 18:46:01 -0500

    new product view

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -8,17 +8,35 @@ class Admin::ProductsController < AdminController
   end
 
   def new
+    @product = Product.new
   end
 
   def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to admin_product_path(@product), notice: "Product Created!"
+    else
+      flash.now[:notice] = "Failed to create product."
+      render :new
+    end
   end
 
   def edit
+    @product = Product.find(params[:id])
   end
 
   def update
+    @product = Product.find(params[:id])
+    @product.update(product_params)
   end
 
   def destroy
+    @product = Product.find(params[:id])
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name,:price,:category_id,:sku,:description)
   end
 end
