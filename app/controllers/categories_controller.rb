d92e264cdad7c7e95e7b3587a6b0af415a569c84
Joshua Masland-Sarani <j.masland@gmail.com>
2016-02-03 14:05:52 -0500

    show category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,4 +2,36 @@ class CategoriesController < ApplicationController
   def new
     @category = Category.new
   end
+
+  def create
+    @category = Category.new( category_params )
+    if @category.save
+      redirect_to admin_path, notice: "Category Created"
+    else
+      flash.now[:alert] = "Failed to Create Category"
+      render :new
+    end
+  end
+
+  def show
+    @category = Category.find( params[:id] )
+  end
+
+  def edit
+    @category = Category.find( params[:id] )
+  end
+
+  def update
+    @category = Category.find( params[:id] )
+  end
+
+  def destroy
+    @category = Category.find( params[:id] )
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
 end
