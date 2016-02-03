4bed246fe12e47f344425fb0439bcdccff6c6778
Andrew Baik <and.baik@gmail.com>
2016-02-02 19:55:44 -0500

    panel 5 stuff

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,88 +1,18 @@
 class DashboardController < ApplicationController
+  include Analytics
 
   def index
-
     begin_time = Time.now
     puts "Begin time: #{begin_time}"
 
-    a = AnalyticsHelper.new
-
-    a.panel_one
-
-    # Top 3 States
-    states_query = State.top_three_states
-    @three_states = {}
-
-    states_query.each do |state|
-      @three_states[state.name] = state.state_count
-    end
-
-    # Top 3 Cities
-    cities_query = City.top_three_cities
-    @three_cities = {}
-
-    cities_query.each do |city|
-      @three_cities[city.name] = city.city_count
-    end
-
-    # Top User With...
-    highest_single_order = User.highest_single_order
-    highest_lifetime = User.highest_lifetime
-    highest_average = User.highest_average
-    most_ordered = User.most_ordered
-
-    @data = [
-      ["Item", "User", "Quantity"],
-      ["Highest Single Order Value", highest_single_order.first.user, highest_single_order.first.order_value],
-      ["Highest Lifetime Value", highest_lifetime.first.user, highest_lifetime.first.lifetime_total],
-      ["Highest Average Order Value", highest_average.first.user, highest_average.first.avg_order_value],
-      ["Most Orders Placed", most_ordered.first.user, most_ordered.first.order_count]
-    ]
-
-    @order_seven_days = {
-      "Number of Orders" => @overall_seven_days["Orders"],
-      "Total Revenue" => @overall_seven_days["Revenue"],
-      "Average Order Value" => Order.average_order(7),
-      "Largest Order Value" => Order.largest_order(7).first.largest_order
-    }
-
-    @order_thirty_days = {
-      "Number of Orders" => @overall_thirty_days["Orders"],
-      "Total Revenue" => @overall_thirty_days["Revenue"],
-      "Average Order Value" => Order.average_order(30),
-      "Largest Order Value" => Order.largest_order(30).first.largest_order
-    }
-
-    @order_totals = {
-      "Number of Orders" => Order.orders_total,
-      "Total Revenue" => Order.revenue_total,
-      "Average Order Value" => Order.average_total,
-      "Largest Order Value" => Order.largest_total
-    }
-
-    daily_timeseries = Order.daily_timeseries_orders
-    @daily_timeseries_data = [
-      ["Date", "Quantity", "Value"]
-    ]
-    daily_timeseries.each do |day|
-      @daily_timeseries_data << [day.day, day.num_orders, day.daily_sum]
-    end
-
-
-
-    weekly_timeseries = Order.weekly_timeseries_orders
-    @weekly_timeseries_data = [
-      ["Date", "Quantity", "Value"]
-    ]
-    weekly_timeseries.each do |week|
-      @weekly_timeseries_data << [week.week, week.num_orders, week.weekly_sum]
-    end
-
+    panel_one
+    panel_two
+    panel_three
+    panel_four
+    panel_five
 
     end_time = Time.now
     puts "Time taken is #{end_time - begin_time}."
-
-
   end
 
   def get
