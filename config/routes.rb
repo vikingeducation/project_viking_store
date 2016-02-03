30b5cc397ba6e7d3515cdb644d24929f5175c7a3
Julia Herron <julia.herron@gmail.com>
2016-02-03 11:18:45 -0800

    a lot of work just to move dashboard to /admin lol

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
 
 
   root 'dashboard#index'
-  resources :dashboard, only: [:index, :get]
+
+  scope "/admin" do
+    resources :dashboard, only: [:index, :get]
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
