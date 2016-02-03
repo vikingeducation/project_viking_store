7d4ccd085248ded87bcb270eb530fed30b6aa5e9
koziscool <kozjob@yahoo.com>
2016-02-03 16:42:15 -0600

    started bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
 
   root 'dashboards#index'
   get '/dashboard' => 'dashboards#index'
+  get '/admin' => 'admins#index'
+
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
