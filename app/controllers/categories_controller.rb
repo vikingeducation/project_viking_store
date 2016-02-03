f664c6a414853b7abb6efcfe532db2009fb0441b
Andrew Baik <and.baik@gmail.com>
2016-02-03 17:39:48 -0500

    finished up to category update action

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,10 +2,39 @@ class CategoriesController < ApplicationController
 
   def index
     @categories = Category.all
-  end  
+  end
 
 
   def new
     @category = Category.new
-  end  
+  end
+
+  def show
+    @category = Category.find(params[:id])
+    @products = Category.get_all_products(params[:id])
+  end
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      flash[:success] = "You've Sucessfully Updated the Category!"
+      redirect_to category_path(@category)
+    else
+      flash.now[:error] = "Error! Category wasn't be updated!"
+      render :edit
+    end
+  end
+
+  def destory
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
 end
