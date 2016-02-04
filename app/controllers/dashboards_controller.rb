b9f118943be41dec7064101c2b45638a98421fa5
Thomas Lo <thomasjinlo@gmail.com>
2016-02-03 17:04:31 -0800

    Finish tables

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -26,7 +26,13 @@ class DashboardsController < ApplicationController
   end
 
   def new
-    @users = User.all
+    @users = User.user_data.limit(9)
+
+    @last_date = {}
+
+    @users.each do |user|
+      @last_date[user.id] = User.last_order_date(user.id)
+    end
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -16,4 +16,31 @@ class User < ActiveRecord::Base
     self.select("COUNT(*) AS user_count, cities.name").joins("JOIN addresses ON addresses.user_id = users.id").joins("JOIN cities ON cities.id = addresses.city_id").group("cities.name").order("user_count DESC").limit(3)
   end
 
+  def self.user_data
+    self.select("users.id AS ID, users.first_name AS Name, users.created_at AS Joined, cities.name AS City, states.name AS state, COUNT(orders.id) AS Orders").
+    joins("JOIN addresses ON addresses.id = users.billing_id").
+    joins("JOIN cities ON addresses.city_id = cities.id").
+    joins("JOIN states ON addresses.state_id = states.id").
+    joins("JOIN orders ON orders.user_id = users.id").
+    group("users.id, city, state").
+    order("ID")
+  end
+
+
+  def self.last_order_date(id)
+    self.select("orders.checkout_date AS date").
+    joins("JOIN orders ON orders.user_id = users.id").
+    where("orders.checkout_date IS NOT NULL AND users.id = #{id}").
+    order("orders.checkout_date DESC").
+    limit 1
+  end
+
+
+# SELECT users.id as userss, orders.id as orderss, orders.checkout_date
+# FROM users JOIN orders ON orders.user_id = users.id
+# WHERE orders.checkout_date IS NOT NULL AND users.id = 2
+# GROUP BY users.id, orders.id
+# ORDER BY orders.checkout_date DESC 
+# LIMIT 1
+
 end
