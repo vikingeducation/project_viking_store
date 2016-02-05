a9ed7bf3e53c34c48fd0349661b40204f2b870b4
Julia Herron <julia.herron@gmail.com>
2016-02-05 13:46:10 -0800

    added telephone number for users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150728230632) do
+ActiveRecord::Schema.define(version: 20160205213710) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -105,6 +105,7 @@ ActiveRecord::Schema.define(version: 20150728230632) do
     t.integer  "shipping_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "telephone"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
