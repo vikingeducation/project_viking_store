a2a69f4a1316bbdf6723ffd7b42233dff35dbfe5
Andrew Baik <and.baik@gmail.com>
2016-02-05 20:03:24 -0500

    create address feature, need to finish collection select on value

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -2,6 +2,28 @@ class AddressesController < ApplicationController
 
   def index
     @addresses = Address.all
+    @this_user = params[:this_user].to_i
+    if @this_user != 0
+      @this_user = User.find(@this_user)
+      @addresses = @this_user.addresses
+    end
   end
 
+  def new
+    @address = Address.find(params[:user_id])
+  end
+
+  def show
+    @address = Address.find(params[:id])
+  end
+
+  def edit
+    @address = Address.find(params[:id])
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end
