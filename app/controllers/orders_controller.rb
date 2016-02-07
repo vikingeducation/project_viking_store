865f3a537e1d96cc3763248ae6a5462f4af050ff
Andrew Baik <and.baik@gmail.com>
2016-02-07 00:42:13 -0500

    orders index working per user

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,10 @@
 class OrdersController < ApplicationController
   def index
     @orders = Order.all.order("orders.id")
+    @user = params[:user_id].to_i
+    if @user != 0
+      @user = User.find(@user)
+      @orders = @user.orders
+    end
   end
 end
