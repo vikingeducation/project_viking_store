e176eeefd58bceb2209f3164b6c86ee68072592b
Kelsey James <kelseybjames@gmail.com>
2016-02-09 16:56:35 -0800

    TODO figure out user session, figure out category dropdown

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,7 @@
 class ProductsController < ApplicationController
 
   def index
+    @category_options = Category.all.map{|c| [c.name, c.id]}
     @category = Category.first
     @products = Product.where(category_id: @category.id )
   end
