68856bc96dec0ba844ccb334019064b95aabf9b6
Julia Herron <julia.herron@gmail.com>
2016-02-10 16:17:20 -0800

    login, logout, more users stuff

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,10 @@
+class OrdersController < ApplicationController
+
+  def new
+    @order = Order.new
+  end
+
+
+
+
+end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,11 +5,28 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by_email(params[:email])
-
+    user = User.find_by_email(params["/sessions"][:email])
+    if user
+      sign_in_user(user)
+      flash[:success] = "Signed In"
+      redirect_to products_path
+    else
+      flash.now[:error] = "Failed to sign in"
+      render :new
+    end
   end
 
 
+
+  def destroy
+    if sign_out
+      flash[:success] = "You have been signed out"
+
+    else
+      flash[:error] = "Failed to sing out"
+    end
+    redirect_to products_path
+  end
 
 
 
