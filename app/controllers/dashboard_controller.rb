84cb114a4c4ccacf23ea8050132c1928bd460fcd
Steven Chang <prime_pork@hotmail.com>
2016-03-15 17:47:36 +1000

    controller code has been nicely spaced out and commented

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,18 +1,27 @@
 class DashboardController < ApplicationController
 
   def index
+    # Panel 1
+    # Totals Table
     @total_users = User.all.count
     @total_orders = Order.where("checkout_date IS NOT NULL").count
     @total_products = Product.all.count
     @total_revenue = OrderContent.total_revenue_since_days_ago(100000).first.total
+
+    # Last 30 Days Table
     @users_last_thirty_days = User.created_since_days_ago(30)
     @orders_last_thirty_days = Order.created_since_days_ago(30)
     @products_last_thirty_days = Product.created_since_days_ago(30)
     @total_revenue_last_thirty_days = OrderContent.total_revenue_since_days_ago(30).first.total
+
+    # Last 7 Days Table
     @users_last_seven_days = User.created_since_days_ago(7)
     @orders_last_seven_days = Order.created_since_days_ago(7)
     @products_last_seven_days = Product.created_since_days_ago(7)
     @total_revenue_last_seven_days = OrderContent.total_revenue_since_days_ago(7).first.total
+
+    # Panel 2
+    # Top 3 States
     top_three_states = State.top_three_states
     @best_state_name = top_three_states.first.name
     @best_state_total = top_three_states.first.total
@@ -20,6 +29,8 @@ class DashboardController < ApplicationController
     @second_state_total = top_three_states.second.total
     @third_state_name = top_three_states.third.name
     @third_state_total = top_three_states.third.total
+
+    # Top 3 Cities
     top_three_cities = City.top_three_cities
     @best_city_name = top_three_cities.first.name
     @best_city_total = top_three_cities.first.total
@@ -27,20 +38,27 @@ class DashboardController < ApplicationController
     @second_city_total = top_three_cities.second.total
     @third_city_name = top_three_cities.third.name
     @third_city_total = top_three_cities.third.total
+
+    # Top Users
     biggest_single_order = User.biggest_single_order
     @biggest_order_name = "#{biggest_single_order.first.first_name} #{biggest_single_order.first.last_name}"
     @biggest_order_amount = biggest_single_order.first.amount.to_s
+
     biggest_lifetime_spender = User.biggest_lifetime_spender
     @biggest_lifetime_spender_name = "#{biggest_lifetime_spender.first.first_name} #{biggest_lifetime_spender.first.last_name}"
     @biggest_lifetime_spender_amount = biggest_lifetime_spender.first.amount.to_s
+
     biggest_average_spender = User.biggest_average_spender
     @biggest_average_spender_name = "#{biggest_average_spender.first.first_name} #{biggest_average_spender.first.last_name}"
     @biggest_average_spender_amount = biggest_average_spender.first.average_order.to_s
+
     most_orders_placed = User.most_orders_placed
     @most_orders_placed_name = "#{most_orders_placed.first.first_name} #{most_orders_placed.first.last_name}"
     @most_orders_placed_amount = most_orders_placed.first.total_orders
-    @average_order_value_total = OrderContent.average_order_value(100000)
 
+    # Panel 3
+    @biggest_order_amount_last_thirty_days = OrderContent.biggest_order(30).first.amount
+    @biggest_order_amount_last_seven_days = OrderContent.biggest_order(7).first.amount
   end
 
 end
--- app/models/order_content.rb
+++ app/models/order_content.rb
@@ -7,12 +7,13 @@ class OrderContent < ActiveRecord::Base
   # Do this by combining order_contents table and products table
   # Then joining the orders table on so that we can filter our items that have a checkout_date after the date we've specified.
 
-  def self.total_revenue_since_days_ago(number)
-    date = Time.now - number.days
+  def self.total_revenue_since_days_ago(number_of_days)
+    date = Time.now - number_of_days.days
     OrderContent.find_by_sql("SELECT SUM(order_contents.quantity * products.price) as total FROM order_contents JOIN products ON order_contents.product_id=products.id JOIN orders ON orders.id=order_contents.order_id WHERE orders.checkout_date >= '#{date}'")
   end
 
-  def self.average_order_value(number)
-    (self.total_revenue_since_days_ago(number).first.total / Order.where("checkout_date IS NOT NULL").count).round(2)
+  def self.biggest_order(number_of_days)
+    date = Time.now - number_of_days.days
+    OrderContent.find_by_sql("SELECT order_contents.order_id, SUM(order_contents.quantity * products.price) as amount FROM orders JOIN order_contents ON orders.id=order_contents.order_id JOIN products ON order_contents.product_id=products.id WHERE orders.checkout_date > '#{date}' GROUP BY order_contents.order_id ORDER BY amount DESC LIMIT 1")
   end
 end
