438a114954e580aafdc6cf1cd10bc764a5b53887
Steven Chang <prime_pork@hotmail.com>
2016-03-28 16:30:59 +1000

    made the beginnings of the users show method and show page. The users name and id is across the top

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,11 @@ class UsersController < ApplicationController
     @column_names = ["ID","Name","Joined","City","State","Orders","Last Order Date", "SHOW", "EDIT", "DELETE"]
     @users = User.all_in_arrays
   end
+
+  def show
+    @user = User.find(params[:id])
+    # @category_name = Product.category_name(@product.category_id)
+    # @times_ordered = Product.times_ordered(params[:id])
+    # @number_of_carts_in = Product.number_of_carts_in(params[:id]).first.total
+  end
 end
