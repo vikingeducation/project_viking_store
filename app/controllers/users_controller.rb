bec8a852e587cb57e2e8b534bd4e21a4bdfc5542
Steven Chang <prime_pork@hotmail.com>
2016-03-31 17:28:26 +1000

    new user form renders

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,18 @@ class UsersController < ApplicationController
   
   layout 'admin_portal'
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
   def index
     @column_headers = ["ID","Name","Joined","City","State","Orders","Last Order Date", "SHOW", "EDIT", "DELETE"]
     @users = User.all
+  end
+
+  def new
+    @user = User.new
+    @user_orders_table_headers = ["ID","Order Date","Order Value","Status","SHOW", "EDIT", "DELETE"]
   end
 
   def show
