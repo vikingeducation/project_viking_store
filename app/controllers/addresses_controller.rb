2566021224835b137d4a63e9260f3d1342460963
Steven Chang <prime_pork@hotmail.com>
2016-04-02 07:27:45 +1000

    addresses index page - orders shipped to and created_at columns added

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -3,7 +3,7 @@ class AddressesController < ApplicationController
   layout "admin_portal"
 
   def index
-    @column_headers = ["ID","User","Address","City","State","Orders","Created","SHOW","EDIT","DELETE"]
+    @column_headers = ["ID","User","Address","City","State","Orders Shipped To","Created","SHOW","EDIT","DELETE"]
     @addresses = Address.all
   end
 
