27ce1ea18dde785cd976598655ea48bd1531d8d7
Steven Chang <prime_pork@hotmail.com>
2016-04-04 06:38:31 +1000

    when creating a new address, it now adds the user_id to the address

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -2,6 +2,17 @@ class AddressesController < ApplicationController
 
   layout "admin_portal"
 
+  def create
+    @address = Address.new(whitelisted_params.merge({:user_id => 1004}))
+    if @address.save 
+      redirect_to :addresses
+      flash[:notice] = "New Address Created!"
+    else
+      flash.now[:alert] = "New Address Could Not Be Created, Please Try Again."
+      render :new_a
+    end
+  end
+
   def index
     @column_headers = ["ID","User","Address","City","State","Orders Shipped To","Created","SHOW","EDIT","DELETE"]
     @addresses = Address.all
@@ -20,6 +31,21 @@ class AddressesController < ApplicationController
     @user_name = User.find(params[:user_id]).full_name
     @column_headers = ["ID","User","Address","City","State","Orders Shipped To","Created","SHOW","EDIT","DELETE"]
     @addresses = Address.where(:user_id => params[:user_id])
+  end
+
+  private
+
+  def city_id_hash
+    if City.where(:name => params["address"]["city"].titleize).first
+      {:city_id => City.where(:name => params["address"]["city"].downcase.titleize).first.id}
+    else
+      new_city = City.create(name: params["address"]["city"].downcase.titleize)
+      {:city_id => new_city.id}
+    end
+  end
+
+  def whitelisted_params
+    params.require(:address).permit(:street_address,:zip_code,:state_id).merge(city_id_hash)
   end
 
 end
