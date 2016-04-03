1912903f69d8cdace59aebd81292a3943d6aff37
Steven Chang <prime_pork@hotmail.com>
2016-04-03 15:06:20 +1000

    on addresses for_user page, I changed the button_to to a link_to and it now goes to the new address page for that user

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -2,19 +2,24 @@ class AddressesController < ApplicationController
 
   layout "admin_portal"
 
-  def for_user
-    @user_name = User.find(params[:user_id]).full_name
-    @column_headers = ["ID","User","Address","City","State","Orders Shipped To","Created","SHOW","EDIT","DELETE"]
-    @addresses = Address.where(:user_id => params[:user_id])
-  end
-
   def index
     @column_headers = ["ID","User","Address","City","State","Orders Shipped To","Created","SHOW","EDIT","DELETE"]
     @addresses = Address.all
   end
 
+  def new
+    @user = User.find(params[:user_id])
+    @address = Address.new
+  end
+
   def show
     @address = Address.find(params[:id])
+  end
+
+  def for_user
+    @user_name = User.find(params[:user_id]).full_name
+    @column_headers = ["ID","User","Address","City","State","Orders Shipped To","Created","SHOW","EDIT","DELETE"]
+    @addresses = Address.where(:user_id => params[:user_id])
   end
 
 end
