2dc211254b6ff4abe54a4f084177643e01a0be1e
Steven Chang <prime_pork@hotmail.com>
2016-04-05 09:14:52 +1000

    orders pointed layout to admin_portal. index page shows table and all columns as per the mockup

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,9 @@
 class OrdersController < ApplicationController
 
+  layout 'admin_portal'
+
   def index
+    @column_headers = ["ID", "UserID", "Address", "City", "State", "Total Value", "Status", "Date Placed", "SHOW", "EDIT", "DELETE"]
     @orders = Order.all
   end
 
