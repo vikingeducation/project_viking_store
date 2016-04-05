943f2d5f19ace28e89a7b84ebd7eb8845dd0ebcf
Steven Chang <prime_pork@hotmail.com>
2016-04-05 10:59:29 +1000

    addresses index - an invalid user_id renders all orders with an error flash

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,8 +5,14 @@ class OrdersController < ApplicationController
   def index
     @column_headers = ["ID", "UserID", "Address", "City", "State", "Total Value", "Status", "Date Placed", "SHOW", "EDIT", "DELETE"]
     if params[:user_id]
-      @orders = Order.where(:user_id => params[:user_id])
-      @orders_index_header = "#{User.find(params[:user_id]).full_name}'s Orders"
+      unless Order.where(:user_id => params[:user_id]).size == 0
+        @orders = Order.where(:user_id => params[:user_id])
+        @orders_index_header = "#{User.find(params[:user_id]).full_name}'s Orders"
+      else
+        flash[:notice] = "Sorry, that user isn't in our system :("
+        @orders = Order.all
+        @orders_index_header = "Orders"
+      end
     else
       @orders = Order.all
       @orders_index_header = "Orders"
