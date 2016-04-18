ab3ae70811e1f82ab2166e8769d9cb253a252df8
Steven Chang <prime_pork@hotmail.com>
2016-04-18 14:24:15 +1000

    general products index page displays all products, 3 per row and you can filter by categories

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,5 +3,8 @@ class ProductsController < ApplicationController
   layout 'front_facing'
 
   def index
+    @category_options = Category.all.map{|u| [u.name,u.id]}
+    params[:category_id] ? @products = Product.where(:category_id => params[:category_id]) : @products = Product.all
+    @selected_option = params[:category_id] if params[:category_id]
   end
 end
