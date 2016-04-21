852ebf771a3b351463b3a1e1f3965d8e30ea15e6
Steven Chang <prime_pork@hotmail.com>
2016-04-21 20:02:06 +1000

    general users controller - added a method add_city_id_to_params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,17 @@ class UsersController < ApplicationController
   layout 'front_facing'
 
   def create
-    # The first problem is trying to figure out how I'm gonna get a city_id into this thing....
+    add_city_id_to_params
+    @user = User.new(whitelisted_params)
+  end
+
+  def new
+    @user = User.new
+  end
+
+  private
+
+  def add_city_id_to_params
     # All the addresses keys are from 0 upwards which means we could iterate with a .times enumerator pretty easy
     params["user"]["addresses_attributes"].size.times do |x| 
       # This will return the city that was entered for the address with x as the key.
@@ -13,18 +23,9 @@ class UsersController < ApplicationController
       # Entering that into the address city_id
       address[:city_id] = city_id
     end
-    @user = User.new(whitelisted_params)
-    asdfa
   end
-
-  def new
-    @user = User.new
-  end
-
-  private
 
   def whitelisted_params
-
     # Hopefully i can just require and permit this now...
     params.require(:user).permit(:billing_id, :shipping_id, :email, :first_name, :last_name, :addresses_attributes => [:id, :street_address, :state_id, :zip_code, :city_id])
   end
