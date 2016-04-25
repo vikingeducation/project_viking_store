1a92dfc82cd7416c0e210158d174f41aca6cc4f9
Steven Chang <prime_pork@hotmail.com>
2016-04-25 11:35:45 +1000

    some code refactoring

--- app/controllers/shopping_carts_controller.rb
+++ app/controllers/shopping_carts_controller.rb
@@ -16,33 +16,47 @@ class ShoppingCartsController < ApplicationController
     # Getting the order contents before deleting the session shopping cart items.
     order_contents_before_update = order_contents
     session[:shopping_cart_items] = []
+
     order_contents_before_update.each_with_index do |order_content,index|
       order_content.quantity = params["order_content_#{index}"]["quantity"]
       store_or_destroy_product(order_content, index)
     end
+
+    redirect_after_cart_update
+  end
+
+  private
+
+  def redirect_after_cart_update
     if cart_is_empty?
       redirect_to shoppingcart_path
       flash[:notice] = "You ain't got no items in your blimey cart."
     else
       # This code is for the two submit buttons. One will be to update contents, the one for the checkout
       if params[:commit] == "Update Contents"
-        @order_contents = order_contents
-        @order_total = order_total(@order_contents)
-        flash.now[:alert] = "Cart updated."
-        render :edit
+        redirect_back_to_cart
       else
-        # If the user's signed in, we're going to the edit_order_path, otherwise they're redirected to the new_session_path.
-        if signed_in_user?
-          redirect_to edit_order_path(:id => current_user.orders.where(:checkout_date => nil).first.id)
-        else
-          flash[:alert] = "Please sign in or sign up to check out."
-          redirect_to new_session_path
-        end
+        redirect_to_checkout
       end
     end
   end
 
-  private
+  def redirect_back_to_cart
+    @order_contents = order_contents
+    @order_total = order_total(@order_contents)
+    flash.now[:alert] = "Cart updated."
+    render :edit
+  end
+
+  def redirect_to_checkout
+    # If the user's signed in, we're going to the edit_order_path, otherwise they're redirected to the new_session_path.
+    if signed_in_user?
+      redirect_to edit_order_path(:id => current_user.orders.where(:checkout_date => nil).first.id)
+    else
+      flash[:alert] = "Please sign in or sign up to check out."
+      redirect_to new_session_path
+    end
+  end
 
   def cart_is_empty?
     if signed_in_user?
