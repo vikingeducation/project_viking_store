af6266db8dea273ab286cb451eb044dfa731f922
Steven Chang <prime_pork@hotmail.com>
2016-04-25 10:37:34 +1000

    on the cart page, clicking on checkout takes you to the checkout page. If user is not signed in, it takes them to the sign in page. If the user tries to pop a order into the params that isn't their cart id, the user is redirected to their cart.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,7 +2,21 @@ class OrdersController < ApplicationController
 
   layout 'front_facing'
 
+  # First off, I want this to only work if the user is currently signed in and only if the order belongs to the signed_in user... 
   def edit
+    # First - making sure that the user is signed in.
+    if signed_in_user?
+      # Second - making sure the order in the params is that user's shopping cart
+      if current_user.orders.where(:checkout_date => nil).first.id == params[:id].to_i
+        @order = Order.find(params[:id])
+      else
+        flash[:alert] = "You can only checkout your current shopping cart."
+        redirect_to shoppingcart_path
+      end
+    else
+      flash[:alert] = "Please sign in or sign up to check out."
+      redirect_to new_session_path
+    end
   end
 
 end
