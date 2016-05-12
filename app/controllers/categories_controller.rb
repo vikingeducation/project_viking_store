be2ab189f24f22ace4b67b8a7a7b4bd5d07b14fc
JeremyVe <jepschx@gmail.com>
2016-05-12 15:51:17 +0800

    edit category done

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -14,15 +14,30 @@ class CategoriesController < ApplicationController
       flash[:success] = "You just add a new Category named #{@category.name.titleize}"
       redirect_to categories_path
     else
-      flash.now[:danger] = "Something went weird, You didn't save anything"
+      flash.now[:danger] = "Something went wrong, You didn't save anything"
       render :new
     end
   end
 
-    def show
-      @category = Category.find(params[:id])
-      @products_of_category = Product.by_category(@category.id)
+  def show
+    @category = Category.find(params[:id])
+    @products_of_category = Product.by_category(@category.id)
+  end
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(whitelisted_params)
+      flash[:success] = "You Succesfully Edit the Category"
+      redirect_to categories_path
+    else
+      flash.now[:danger] = "Something whent wrong, You should double check"
+      render :edit
     end
+  end
 
   private
 
