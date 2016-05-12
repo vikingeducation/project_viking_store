3f3083fdcf42919fced3f6f2dab13a87bd3c75c1
JeremyVe <jepschx@gmail.com>
2016-05-12 15:21:19 +0800

    category new done

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,4 +3,26 @@ class CategoriesController < ApplicationController
   def index
     @categories = Category.all
   end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(whitelisted_params)
+    if @category.save
+      flash[:success] = "You just add a new Category named #{@category.name.titleize}"
+      redirect_to categories_path
+    else
+      flash.now[:danger] = "Something went weird, You didn't save anything"
+      render :new
+    end
+
+  end
+
+  private
+
+  def whitelisted_params
+    params.require(:category).permit(:name)
+  end
 end
