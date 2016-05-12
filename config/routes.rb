1377072005424fca98cedf8d02f1b1e520a3c798
JeremyVe <jepschx@gmail.com>
2016-05-12 14:53:20 +0800

    added category page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   root 'admin_board#index'
+
+  resources :categories
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
