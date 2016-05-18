9e03dc421d57b2cc6e0f28f3aa5f1c91d8dc4bd2
JeremyVe <jepschx@gmail.com>
2016-05-18 09:04:52 +0800

    filter index user

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,5 +1,8 @@
 class AddressesController < ApplicationController
   def index
     @addresses = Address.all
+    if params[:user_id] && User.exists?(params[:user_id])
+      @user = User.find(params[:user_id])
+    end
   end
 end
