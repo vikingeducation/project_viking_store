8fb55fdf337e97bee55cc3556ea0828a355d1467
JeremyVe <jepschx@gmail.com>
2016-05-27 15:39:33 +0800

    added category filter

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,6 +2,14 @@ class ProductsController < ApplicationController
   layout "shop"
 
   def index
-    @products = Product.all
+    category_id = params[:filter_category].to_i
+    if Category.ids.include?(category_id)
+      @products = Product.where(category_id: category_id)
+      @filter_category = Category.find(category_id).name
+    else
+      @products = Product.all
+      @filter_category = "Choose a Category"
+    end
+
   end
 end
