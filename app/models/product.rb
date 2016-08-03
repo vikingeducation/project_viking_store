5dba5c3d044d7a742cc0332c3a331eba69efe5f4
Vishal Lama <vishal.lama@gmail.com>
2016-08-03 12:43:37 -0600

    Work on get_revenue

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,6 @@
 class Product < ActiveRecord::Base
   scope :last_seven_days, -> { get_created_at(7.days.ago) }
   scope :last_thirty_days, -> { get_created_at(30.days.ago) }
-  scope :revenue_last_seven_days, -> { get_revenue(7.days.ago) }
-  scope :revenue_last_thirty_days, -> { get_revenue(30.days.ago) }
 
   class << self
     
@@ -12,15 +10,11 @@ class Product < ActiveRecord::Base
         where("created_at <= ?", time).count
       end
 
-      def get_revenue(time)
-        Product.where("created_at <= ?", time).sum(:price)
-      end
-
   end
 
 end
 
 
-# SELECT * FROM orders WHERE orders.checkout_date IS NOT NULL
-# INNER JOIN order_contents ON orders.id = order_contents.order_id
-# JOIN products ON order_contents.product_id = products.id
+SELECT * FROM orders WHERE orders.checkout_date IS NOT NULL
+INNER JOIN order_contents ON orders.id = order_contents.order_id
+JOIN products ON order_contents.product_id = products.id
