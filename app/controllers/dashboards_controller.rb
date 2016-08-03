db8bdbc907d38e8edf48a91c8b017b08fffc1d9b
Philip Cole Johnson <philipcolejohnson@gmail.com>
2016-08-03 12:04:00 -0700

    Total table

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,12 +1,18 @@
 class DashboardsController < ApplicationController
-  
+
 
   def show
   	@total_users = User.total_users
   	@total_orders = Order.total_orders
   	@total_products = Product.total_products
+
+    @new_week_users = User.total_users(1.week.ago)
+    @new_week_orders = Order.total_orders(1.week.ago)
+    @new_week_products = Product.total_products(1.week.ago)
   	# @total_revenue =
-  	@new_month_user = User.total_users(1.month.ago)
+  	@new_month_users = User.total_users(1.month.ago)
+    @new_month_orders = Order.total_orders(1.month.ago)
+    @new_month_products = Product.total_products(1.month.ago)
   end
 
 
