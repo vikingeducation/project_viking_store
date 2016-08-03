dc21527abb4f25f7117d3eec6dc32f0fa36fca33
Adrian Mui <adrian.mui@gmail.com>
2016-08-03 15:05:15 -0400

    revenue working

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -3,7 +3,7 @@ class DashboardsController < ApplicationController
     @all_time_count = {"Users" => User.get_count,
                                     "Orders" => Order.get_count,
                                     "Products" => Product.get_count,
-                                    "Revenue" => 0 }
+                                    "Revenue" => OrderContent.get_revenue }
     @seven_days_count =   {"Users" => User.get_count(7),
                                     "Orders" => Order.get_count(7),
                                     "Products" => Product.get_count(7),
@@ -16,4 +16,3 @@ class DashboardsController < ApplicationController
   end
 end
 
-OrderContent.find_by_sql("SELECT products.price FROM order_contents JOIN products ON order_contents.product_id = products.id JOIN orders ON orders.id = WHERE order_contents.id = 4")
--- app/models/order_content.rb
+++ app/models/order_content.rb
@@ -1,6 +1,22 @@
 class OrderContent < ActiveRecord::Base
 
-  
+  def self.get_revenue(time = nil)
+    result = OrderContent.select("SUM(quantity * price) AS revenue_sum")
+                          .joins("JOIN products ON order_contents.product_id = products.id")                          
+                          .joins("JOIN orders ON order_contents.order_id = orders.id")
+                          .where("checkout_date IS NOT NULL")
+                          #.group("order_contents.id")
+    result.to_a.first.revenue_sum.to_f
+
+    # result = OrderContent.find_by_sql(
+    #       "SELECT SUM(quantity * price)
+    #       FROM order_contents
+    #       JOIN products ON order_contents.product_id = products.id
+    #       JOIN orders ON order_contents.order_id = orders.id
+    #       WHERE checkout_date IS NOT NULL
+    #       ") 
+    # result.first.sum.to_f
+  end
 
   
 end
