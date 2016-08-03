843a673e4a8fb2b3cbb5c73e5128f9f247bdfc86
Vishal Lama <vishal.lama@gmail.com>
2016-08-03 16:43:08 -0600

    Work OrderContent

--- app/models/order_content.rb
+++ app/models/order_content.rb
@@ -14,7 +14,39 @@ class OrderContent < ActiveRecord::Base
     end
 
     def highest_lifetime_value
-      
+      OrderContent
+        .select("users.first_name, SUM(order_contents.quantity * products.price) AS highest_value")
+        .joins("JOIN orders ON orders.id = order_contents.order_id JOIN products ON order_contents.product_id = product_id JOIN users on users.id = orders.user_id")
+        .where("orders.checkout_date IS NOT NULL")
+        .group("users.id, users.first_name, order_contents.quantity, products.price")
+        .order("highest_value DESC")
+        .limit(1)
+        .first
+        .highest_value
+    end
+
+    def highest_average_value
+      OrderContent
+        .select("users.first_name, AVG(order_contents.quantity * products.price) AS average_value")
+        .joins("JOIN orders ON orders.id = order_contents.order_id JOIN products ON order_contents.product_id = product_id JOIN users on users.id = orders.user_id")
+        .where("orders.checkout_date IS NOT NULL")
+        .group("users.id, users.first_name, order_contents.quantity, products.price")
+        .order("average_value DESC")
+        .limit(1)
+        .first
+        .average_value
+    end
+
+    def most_orders_placed
+      OrderContent
+        .select("users.first_name, SUM(orders.id) AS most_orders_placed")
+        .joins("JOIN orders ON orders.id = order_contents.order_id JOIN products ON order_contents.product_id = product_id JOIN users on users.id = orders.user_id")
+        .where("orders.checkout_date IS NOT NULL")
+        .group("users.id, users.first_name, orders.id")
+        .order("most_orders_placed DESC")
+        .limit(1)
+        .first
+        .most_orders_placed
     end
 
   end
