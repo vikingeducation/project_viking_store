f5eb0390144d74a2170572b1a16146b3ce2df4c6
Philip Cole Johnson <philipcolejohnson@gmail.com>
2016-08-03 16:58:28 -0700

    Removes extra text

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,4 @@ class User < ActiveRecord::Base
 		o = Order.joins("JOIN order_contents ON orders.id = order_contents.order_id").joins("JOIN users ON users.id = orders.user_id").joins("JOIN products ON order_contents.product_id = products.id").group(:order_id, :price, :first_name, :last_name).select("users.first_name, users.last_name, SUM(price * quantity) AS order_cost")
 	end
 
-
-
 end
