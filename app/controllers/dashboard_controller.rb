257a0b14ebaedeca4e6c71a39ffe6e8562aa8c00
cjvirtucio87 <cjv287@gmail.com>
2016-08-03 13:14:42 -0700

    Refactored instance variables.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,16 +1,18 @@
 class DashboardController < ApplicationController
   def show
-    @last_seven_days = {
-      users: User.last_seven_days,
-      orders: Order.last_seven_days,
-      products: Product.last_seven_days,
-      revenue: Order.revenue_last_seven_days
-    }
-    @last_thirty_days = {
-      users: User.last_thirty_days,
-      orders: Order.last_thirty_days,
-      products: Product.last_thirty_days,
-      revenue: Order.revenue_last_thirty_days 
+    @aggregates = {
+      last_seven_days: {
+        users: User.last_seven_days,
+        orders: Order.last_seven_days,
+        products: Product.last_seven_days,
+        revenue: Order.revenue_last_seven_days 
+      },
+      last_thirty_days: {
+        users: User.last_thirty_days,
+        orders: Order.last_thirty_days,
+        products: Product.last_thirty_days,
+        revenue: Order.revenue_last_thirty_days 
+      }
     }
   end
 end
