277652b42036a380badf95f90c86399044d0e22c
Philip Cole Johnson <philipcolejohnson@gmail.com>
2016-08-03 10:55:22 -0700

    Made layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  resource :dashboard, only: :show
+  get 'dashboard' => 'dashboard#show'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
