7f105cd6f65e1424db912df9a95100eaa0f2393d
schoetlr <srlrs20020@gmail.com>
2016-08-12 14:46:14 -0400

    simple refactoring

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,7 @@
 class DashboardController < ApplicationController
   def index
     @orders = Order.all
+    @top_three_cities = User.top_three_cities
+    @top_three_states = User.top_three_states
   end
 end
