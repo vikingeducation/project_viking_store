d0c1023e5d508e34a4a83bb7241c9e30e8e03afd
schoetlr <srlrs20020@gmail.com>
2016-08-14 13:03:28 -0400

    edit product complete

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,8 +8,38 @@ class ProductsController < ApplicationController
   end
 
   def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(whitelisted_params)
+    if @product.save
+      flash[:success] = "Successfully created Product"
+      redirect_to products_path
+    else
+      flash[:error] = "Something went wrong creating your product"
+      redirect_to :back
+    end
   end
 
   def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+
+    if @product.update(whitelisted_params)
+      flash[:success] = "Product successfully updated"
+      redirect_to products_path
+    else
+      flash[:error] = "Something went wrong updating the product"
+      render :edit
+    end 
+  end
+
+  private
+  def whitelisted_params
+    params.require(:product).permit(:name, :description, :sku, :price, :category_id)
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,9 @@ class Product < ActiveRecord::Base
   has_many :order_contents
   has_many :orders, through: :order_contents
 
+  validates :name, :description, :sku, :category_id, :price, :presence => true
+  validates :price, :numericality => { :less_than_or_equal_to => 10_000 }
+
   def self.created_last_seven_days
     Product.where('created_at > ?', Time.now - 7.days)
   end
