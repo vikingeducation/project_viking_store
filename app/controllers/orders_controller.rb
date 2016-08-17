f62eceea8dac5d1706591750afd0fbf3e7e8103f
schoetlr <srlrs20020@gmail.com>
2016-08-17 10:28:18 -0400

    filtered orders by user

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,13 @@
 class OrdersController < ApplicationController
   def index
-    @user = User.find(params[:user_id]) if params[:user_id]
-    @orders = Order.all.limit(100)
+    if params[:user_id]
+      @user = User.find(params[:user_id]) 
+      @orders = Order.where("user_id=?", params[:user_id]).limit(100)
+    else
+      #not going to put a flash error because sometimes you just want to see
+      #all the orders regardless of user
+      @orders = Order.all.limit(100)
+    end
   end
 
   def show
