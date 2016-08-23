26973875ab6fd9b98e04d0f16b45c0da112dd38b
BranLiang <lby89757@hotmail.com>
2016-08-23 13:42:17 +0800

    add category index

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,14 @@
+class CategoriesController < ApplicationController
+  def index
+    @categories = Category.all
+  end
+
+  def new
+  end
+
+  def show
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'dashboard', to: 'dashboards#index'
+  resources :categories, only: [:show, :index, :show, :edit, :destroy]
 end
