a198feb17f6d2205437cc3414e80a14084cc40d6
BranLiang <lby89757@hotmail.com>
2016-08-23 15:37:51 +0800

    finish the category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -19,9 +19,35 @@ class CategoriesController < ApplicationController
   end
 
   def show
+    @category = Category.find(params[:id])
+    @products = Category.all_related_products(params[:id])
   end
 
   def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(white_list_params)
+      flash[:success] = ["Congrats! #{@category.name} updated!"]
+      redirect_to categories_path
+    else
+      flash.now[:danger] = @category.errors.full_messages
+      render :edit
+    end
+  end
+
+  def destroy
+    category = Category.find(params[:id])
+    name = category.name
+    if category.destroy
+      flash[:success] = ["Congrats! #{name} is destroyed."]
+      redirect_to categories_path
+    else
+      flash.now[:danger] = category.errors.full_messages
+      render :edit
+    end
   end
 
   private
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,9 @@
 class Category < ApplicationRecord
   validates :name, :presence => true,
                    :length => { within: 4..16 }
+
+
+  def self.all_related_products(id)
+    Category.select("products.*").joins("JOIN products ON categories.id = category_id").where("category_id = #{id}")
+  end
 end
