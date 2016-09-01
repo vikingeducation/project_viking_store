e293643a2eb7d81f9d6f0ef27ed3f8376cc8248a
Deepak <grc.deepak@gmail.com>
2016-08-31 19:38:11 -0700

    overall stats done

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,9 @@
 class DashboardController < ApplicationController
 
   def index
-
+    @user_count = User.all.count
+    @order_count = Order.all.count
+    @product_count = Product.all.count
+    @total_revenue = Order.total_revenue
   end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,24 @@
 class Order < ActiveRecord::Base
+
+  def self.total_revenue
+    self.join_with_products.sum("order_contents.quantity * products.price")
+  end
+
+  def self.orders_with_values
+    self.select("orders.*, SUM(order_contents.quantity * products.price) AS order_value").
+          join_with_products.
+          group("orders.id")
+  end
+
+  def self.join_with_products
+    join_order_with_order_contents.join_order_contents_with_products
+  end
+
+  def self.join_order_with_order_contents
+    joins("JOIN order_contents ON orders.id = order_contents.order_id")
+  end
+
+  def self.join_order_contents_with_products
+    joins("JOIN products ON order_contents.product_id = products.id")
+  end
 end
