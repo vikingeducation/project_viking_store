344579e5e063b83c945f40fa2d8ddb2d65f80421
Tamal Riedman <triedman99@gmail.com>
2016-10-17 17:49:59 -0700

    Basic Admin Template and Index of Categories

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'categories/index'
+  get 'admin' => 'admin#index'
+  get 'dashboard' => 'dashboard#main'
+
   root 'dashboard#main'
+
+  resources :admin
 end
