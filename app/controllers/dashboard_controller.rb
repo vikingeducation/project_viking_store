126a5b0116eb94699bb3e0bed84aa372aac9dd9c
Farruh <ufarruh@gmail.com>
2016-10-24 11:01:05 -0700

    Statistics

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,12 @@
 class DashboardController < ApplicationController
   def index
   	@users_seven_days = User.where('created_at < ?', 7.days.ago).count
-  	@orders_seven_days = Order.where('created_at < ?', 7.days.ago).count
+  	@orders_seven_days = Order.where('checkout_date > ?', 7.days.ago).count
+
+  	@users_total = User.all.count
+  	@orders_total = Order.all.count
+  	@products_total = Product.all.count
   end
 end
+
+
