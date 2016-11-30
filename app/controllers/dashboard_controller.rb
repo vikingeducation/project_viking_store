5c12530219dc292f9e85fc8ab1059e633a228d07
Francisco Suero <suerofrancisco@yahoo.com>
2016-11-30 12:58:35 -0500

    woo

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,6 @@
 class DashboardController < ApplicationController
+  include DashboardHelper
   def index
-    #
+    @users_seven_days = total_users(7)
   end
 end
--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,2 +1,17 @@
 module DashboardHelper
+  def total_users(days_ago)
+    User.all.where("created_at >  ?", 7.days.ago).count
+  end
+
+  def total_orders(days_ago)
+
+  end
+
+  def total_new_products(days_ago)
+
+  end
+
+  def total_revenue(days_ago)
+
+  end
 end
