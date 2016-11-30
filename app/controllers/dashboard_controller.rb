c157c98d1d0b9f905c17311fafc1f1c125a34a88
Kenneth Kottenstette <kennykotten@gmail.com>
2016-11-30 10:46:22 -0800

    total revenue

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,5 +2,17 @@ class DashboardController < ApplicationController
   include DashboardHelper
   def index
     @users_seven_days = total_users(7)
+    @orders_seven_days = total_orders(7)
+    @products_seven_days = total_new_products(7)
+    @revenue_seven_days = total_revenue(7).take
+
+puts '111'
+puts @revenue_seven_days
+p @revenue_seven_days
+puts '111'
+
+
   end
+
+
 end
--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,17 +1,38 @@
 module DashboardHelper
   def total_users(days_ago)
-    User.all.where("created_at >  ?", 7.days.ago).count
+    User.all.where("created_at >  ?", days_ago.days.ago).count
   end
 
   def total_orders(days_ago)
-
+    Order.all.where("checkout_date >  ?", days_ago.days.ago).count
   end
 
   def total_new_products(days_ago)
-
+    Product.all.where("created_at >  ?", days_ago.days.ago).count
   end
 
   def total_revenue(days_ago)
+    join_table
+    .select("SUM(price * quantity)")
+    .where("orders.created_at > ?", days_ago.days.ago)
 
   end
+
+  def join_table #order to product
+    Order.joins("JOIN order_contents ON orders.id = order_id")
+    .joins("JOIN products ON products.id = product_id")
+  end
+
+
+
 end
+# Order.joins("JOIN order_contents ON orders.id = order_id").joins("JOIN products ON products.id = product_id").select("SUM(price * quantity)")
+#.where("orders.created_at > ?", 7.days.ago)
+
+# Post.joins("JOIN users ON posts.author_id = users.id")
+# Order.joins("JOIN users ON users.id = orders.user_id")
+
+#
+#
+#
+# .select(:price, :quantity, "price * quantity AS revenue").where("orders.created_at > ?", days_ago.days.ago).sum(:revenue)
