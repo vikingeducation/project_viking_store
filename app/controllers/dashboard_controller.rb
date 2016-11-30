08d1bd4bc123d0c33fdcd7b6b3967614a3a74538
Francisco Suero <suerofrancisco@yahoo.com>
2016-11-30 14:37:24 -0500

    active record sucks. So much

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -6,7 +6,18 @@ class DashboardController < ApplicationController
     @products_seven_days = total_new_products(7)
     @revenue_seven_days = total_revenue(7)
 
+    @users_thirty_days = total_users(30)
+    @orders_thirty_days = total_orders(30)
+    @products_thirty_days = total_new_products(30)
+    @revenue_thirty_days = total_revenue(30)
 
+    #update if database exists in 100 years
+    @users_all_time = total_users(36000)
+    @orders_all_time = total_orders(36000)
+    @products_all_time = total_new_products(36000)
+    @revenue_all_time = total_revenue(36000)
+
+    @top_states = top_three_states
   end
 
 
--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -12,16 +12,24 @@ module DashboardHelper
   end
 
   def total_revenue(days_ago)
-    join_table
-    .where("orders.created_at > ?", days_ago.days.ago)
+    join_table_op
+    .where("checkout_date > ?", days_ago.days.ago)
     .sum("quantity * price")
   end
 
-  def join_table #order to product
+  def join_table_op #order to product
     Order.joins("JOIN order_contents ON orders.id = order_id")
     .joins("JOIN products ON products.id = product_id")
   end
 
+  def join_table_ua
+    User.joins("JOIN addresses ON billing_id = addresses.id")
+  end
+
+
+  def top_three_states
+    join_table_ua.joins("JOIN states ON states.id = state_id").select("name").group("name").order("count_name DESC").limit(3).count
+  end
 
 
 end
