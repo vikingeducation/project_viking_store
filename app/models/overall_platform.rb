7661a24bae963cd24ae52233ec8fc336745f6491
Sampson Crowley <sampsonsprojects@gmail.com>
2016-11-30 14:06:31 -0700

    fix dates

--- app/models/overall_platform.rb
+++ app/models/overall_platform.rb
@@ -10,19 +10,19 @@ class OverallPlatform
 
   def self.user_count(num_days)
     users = User.select('COUNT(*) AS count')
-    users = users.where("created_at <= ?", num_days.days.ago) if num_days
+    users = users.where("created_at >= ?", num_days.days.ago) if num_days
     [ (num_days ? "New Users" : "Users"), users[0].count]
   end
 
   def self.order_count(num_days)
     orders = Order.select('COUNT(*) AS count')
-    orders = orders.where("created_at <= ?", num_days.days.ago) if num_days
+    orders = orders.where("created_at >= ?", num_days.days.ago) if num_days
     ["Orders", orders[0].count]
   end
 
   def self.new_products_count(num_days)
     products = Product.select('COUNT(*) AS count')
-    products = products.where("created_at <= ?", num_days.days.ago) if num_days
+    products = products.where("created_at >= ?", num_days.days.ago) if num_days
     [(num_days ? "New Products" : "Products"), products[0].count]
   end
 
@@ -30,7 +30,7 @@ class OverallPlatform
     revenue = Order.select('SUM(price * quantity) AS total')
                    .joins('JOIN order_contents ON (order_id = orders.id)')
                    .joins('JOIN products ON (product_id = products.id)')
-    revenue = revenue.where("checkout_date <= ?", num_days.days.ago) if num_days
+    revenue = revenue.where("checkout_date >= ?", num_days.days.ago) if num_days
     ["Revenue", ActionController::Base.helpers.number_to_currency(revenue[0].total)]
   end
 
