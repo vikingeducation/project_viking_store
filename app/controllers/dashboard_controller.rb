71d9665f1993f6c329506b7d418fdbbbff51e31f
Jessica G <gillan.jessica@gmail.com>
2016-11-30 17:05:59 -0700

    message

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,9 +4,9 @@ class DashboardController < ApplicationController
     @dashboard = Dashboard.new
 
     @overall_platform   = @dashboard.overall_platform
-    @user_demographics  = @dashboard.populate_user_demographics
-    @order_statistics   = @dashboard.populate_order_statistics
-    @time_series_data   = @dashboard.populate_time_series_data
+    @user_demographics  = @dashboard.user_demographics
+    @order_statistics   = @dashboard.order_statistics
+    @time_series_data   = @dashboard.time_series_data
   end
 
 end
--- app/models/dashboard.rb
+++ app/models/dashboard.rb
@@ -9,23 +9,28 @@ class Dashboard
     @time_series_data  = populate_time_series_data
   end
 
-private
+# private
 
     def populate_overall_platform
-      op = Hash.new { |h, k| h[k] = { } }
+      op = Hash.new #{ |h, k| h[k] = { } }
+      # op = Hash.new { |h, k| h[k] =
+      #                 Hash.new{ |h, k| h[k] = {} } }
 
-      op[:last_7_days][:title] = "Last 7 Days"
-      op[:last_7_days][:data] = { new_users: User.get_new_user_count(7),
+      op[:last_7_days]= { title: "Last 7 Days",
+                          data: { new_users: User.get_new_user_count(7),
                                   orders: Order.get_order_count(7),
                                   new_products: Product.get_product_count(7),
                                   revenue: get_revenue(7) }
+                        }
 
-      op[:last_30_days][:title] = "Last 30 Days"
-      op[:last_30_days][:data]  = { new_users: User.get_new_user_count(30),
+      op[:last_30_days]= { title: "Last 30 Days",
+                           data: { new_users: User.get_new_user_count(30),
                                     orders: Order.get_order_count(30),
                                     new_products: Product.get_product_count(30),
                                     revenue: get_revenue(30) }
+                         }
 
+      op[:totals] = {}
       op[:totals][:title] = "Totals"
       op[:totals][:data]  = { new_users: User.get_new_user_count,
                               orders: Order.get_order_count,
@@ -36,7 +41,7 @@ private
     end
 
     def populate_user_demographics
-      ud = Hash.new { |h, k| h[k] = { } }
+      ud = Hash.new { |h, k| h[k] = {} }
 
       ud[:top_3_states][:title] = "Top 3 States Users Live In (billing)"
       ud[:top_3_states][:data]  = { states: get_top_states }
@@ -48,6 +53,7 @@ private
       ud[:top_user_with][:data] = {
                          highest_single_order: highest_single_order
                        }
+      ud
     end
 
     def populate_order_statistics
@@ -74,21 +80,33 @@ private
     end
 
     def get_top_states
-      User.select("states.name, COUNT(*) AS state_count")
+      ts= User.select("states.name, COUNT(*) AS state_count")
           .joins(" JOIN addresses ON billing_id=addresses.id
                    JOIN states ON state_id=states.id")
                    .group("states.name")
                    .order("state_count DESC")
                    .limit(3)
+
+      [
+        [ ts[0]["states.name"], ts[0].state_count],
+        [ ts[1]["states.name"], ts[1].state_count],
+        [ ts[2]["states.name"], ts[2].state_count]
+      ]
     end
 
     def get_top_cities
-      User.select("cities.name, COUNT(*) AS city_count")
-          .joins(" JOIN addresses ON billing_id=addresses.id
+      tc = User.select("cities.name, COUNT(*) AS city_count")
+           .joins(" JOIN addresses ON billing_id=addresses.id
                    JOIN cities ON city_id=cities.id")
                    .group("cities.name")
                    .order("city_count DESC")
                    .limit(3)
+
+       [
+         [ tc[0]["cities.name"], tc[0].city_count],
+         [ tc[1]["cities.name"], tc[1].city_count],
+         [ tc[2]["cities.name"], tc[2].city_count]
+       ]
     end
 
     def highest_single_order
@@ -99,8 +117,7 @@ private
                          .group("first_name, last_name, order_id")
                          .order("total_price DESC")
                          .limit(1)
-      ["Highest Single Order Value", "#{hso.first_name} #{hso.last_name}", hso.first.total_price.to_f]
+
+      ["Highest Single Order Value", "#{hso[0].first_name} #{hso[0].last_name}", hso.first.total_price.to_f]
     end
-
-
 end
