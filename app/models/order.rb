7400f765ddf94cce1ade5495bacf30e4b1731bdc
Sampson Crowley <sampsonsprojects@gmail.com>
2016-11-30 19:11:06 -0700

    fully functional

--- app/models/order.rb
+++ app/models/order.rb
@@ -8,9 +8,10 @@ class Order < ApplicationRecord
       .number_to_currency(avg.average("price * quantity"))
   end
 
-  def self.count_by_days(num_days = nil)
+  def self.count_by_days(num_days = nil, end_date = nil)
     orders = select('COUNT(*) AS o_count')
     orders = orders.by_days(num_days) if num_days
+    orders = orders.less_than_days(end_date) if end_date
     orders[0].o_count
   end
 
@@ -27,15 +28,21 @@ class Order < ApplicationRecord
     ActionController::Base.helpers.number_to_currency(largest[0].total)
   end
 
-  def self.revenue(num_days = nil)
+  def self.revenue(num_days = nil, end_date = nil)
     revenue = select('SUM(price * quantity) AS total')
               .join_products
     revenue = revenue.by_days(num_days) if num_days
+    revenue = revenue.less_than_days(end_date) if end_date
     ActionController::Base.helpers.number_to_currency(revenue[0].total)
   end
 
   def self.by_days(num_days)
     where("checkout_date >= ?", num_days.days.ago)
+  end
+
+  def self.less_than_days(end_date)
+    where("checkout_date <= ?", (end_date ? end_date.days.ago.end_of_day : Date.today.end_of_day))
+
   end
 
   def self.join_products
--- app/models/time_series.rb
+++ app/models/time_series.rb
@@ -1,8 +1,13 @@
 class TimeSeries
+  include ActionView::Helpers::DateHelper
   def self.data(interval = nil)
     val = []
     7.times do |i|
-      val << [Order.count_by_days(i*interval + 1)]
+      val <<  [
+                ((i+1)*interval).days.ago,
+                Order.count_by_days((i+1)*interval, i*interval),
+                Order.revenue((i+1)*interval, i*interval)
+              ]
     end
     val
   end
