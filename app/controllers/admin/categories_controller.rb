e1198d4d76fdfac9c0c395ab5f2468babc2ef550
Anthony Sin <anthony.t.sin@gmail.com>
2017-02-26 18:16:43 -0500

    categories index, new, create actions

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,4 +1,42 @@
 class Admin::CategoriesController < AdminController
   def index
+    @categories = Category.all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.create(whitelisted_category_params)
+    if @category.save
+      flash[:success] = "Category created!"
+      redirect_to admin_categories_path
+    else
+      flash[:error] = "Category could not be created! #{@category.errors.full_messages[0]}."
+      render action: "new"
+    end
+  end
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+  end
+
+  def show
+    @category = Category.find(params[:id])
+  end
+
+  def destroy
+    @category = Category.find(params[:id])
+  end
+
+  private
+
+  def whitelisted_category_params
+    params.require(:category).permit(:name, :description)
   end
 end
