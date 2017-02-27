eaccbd67dd5f4b6a9362a7ded5650c7da50ce881
Yi-Xuan Lau <yixuan.lau@gmail.com>
2017-02-27 22:40:12 +0800

    Add CRUD functions to products

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,48 +1,47 @@
 class CategoriesController < ApplicationController
-  def show
-    @categories = Category.find(params[:id])
-    @products =
-      render layout: 'admin'
-  end
+    def show
+        @categories = Category.find(params[:id])
+        @products =
+            render layout: 'admin'
+    end
+    ``
+    def edit; end
 
-  def edit
-  end
-
-  def new
-    @category = Category.new
-    render layout: 'admin'
-  end
-
-  def create
-    @category = Category.new(whitelisted_categories)
-    if @category.save
-      flash[:success] = 'Your category was created'
-      redirect_to categories_path
-    else
-      flash.now[:error] = "Your category"
-      render :new, layout: 'admin'
+    def new
+        @category = Category.new
+        render layout: 'admin'
     end
 
-  end
-
-  def index
-    @categories = Category.all
-    render layout: 'admin', locals: { rows: @categories, headings: ['id', 'name', 'description']}
-  end
-
-  def destroy
-    @category = Category.find(params[:id])
-    @category.delete
-    redirect_to categories_path
-  end
-
-  private
-
-  def whitelisted_categories
-    if params[:category]
-      params.require(:category).permit(:name, :description)
-    else
-      params.permit(:name, :description)
+    def create
+        @category = Category.new(whitelisted_categories)
+        if @category.save
+            flash[:success] = 'Your category was created'
+            redirect_to categories_path
+        else
+            flash.now[:error] = 'Your category'
+            render :new, layout: 'admin'
+        end
     end
-  end
+
+    def index
+        @categories = Category.all
+        render layout: 'admin', locals: { rows: @categories, headings: %w(id name description) }
+    end
+
+    def destroy
+        @category = Category.find(params[:id])
+        Product.where("category_id = #{params[:id]}").each { |prod| prod.category_id = nil }
+        @category.delete
+        redirect_to categories_path
+    end
+
+    private
+
+    def whitelisted_categories
+        if params[:category]
+            params.require(:category).permit(:name, :description)
+        else
+            params.permit(:name, :description)
+        end
+    end
 end
--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,10 +1,9 @@
 module DashboardHelper
-
-  def human_time(time)
-    case t = Time.now() - time
-    when t < 1 then 'Today'
-    when t < 2 then 'Yesterday'
-    else time.strftime('%m/%d/%Y')
+    def human_time(time)
+        case t = Time.now - time
+        when t < 1 then 'Today'
+        when t < 2 then 'Yesterday'
+        else time.strftime('%m/%d/%Y')
+        end
     end
-  end
 end
