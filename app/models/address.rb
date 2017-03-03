b7d4636ea1ad59572205d98c75fc30280c9b983f
Yi-Xuan Lau <yixuan.lau@gmail.com>
2017-03-03 17:21:38 +0800

    Set up address deletion

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,6 +1,8 @@
 class Address < ApplicationRecord
-  has_one :user, foreign_key: :shipping_id, primary_key: :id, dependent: :nullify
-  has_one :user, foreign_key: :billing_id, primary_key: :id, dependent: :nullify
+  belongs_to :user
+  # note: setting the 'alias' on has_one seems to have helped Rails understand WHICH column to nullify on the User side of things
+  has_one :as_billing, foreign_key: :billing_id, dependent: :nullify, class_name: 'User'
+  has_one :as_shipping, foreign_key: :shipping_id, dependent: :nullify, class_name: 'User'
   belongs_to :city
   belongs_to :state
   has_many :orders, foreign_key: :billing_id, dependent: :nullify
