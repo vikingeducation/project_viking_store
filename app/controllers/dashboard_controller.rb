91621ddcdc8557635ad29202717557e44f9b9944
Visiona <darbis@gmail.com>
2017-04-10 19:08:59 +0100

    Minor corrections to HTML of dashboard and few aggreagated functions created for all_time_totals scenario

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,24 @@
 class DashboardController < ApplicationController
 
   def index
+    @seven_days_users = User.seven_days_users
+
+
+
+# self.total_users
+  
+# self.total_orders
+ 
+# self.total_products
+# self.total_revenue
+
+
+
+
+
+
+
+
     
   end
 end
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,27 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+    def self.seven_days_users
+      User.where('created_at > ?', (Time.zone.now.end_of_day - 7.days)).count
+    end
+
+    def self.total_users
+      User.count
+    end
+
+    def self.total_orders
+      Orders.where('checkout_date != ?', nil ).count
+    end
+
+    def self.total_products
+      Products.count
+    end
+
+    def self.total_revenue
+      User.find_by_sql("
+          SELECT 
+        ")
+    end
+
+
 end
