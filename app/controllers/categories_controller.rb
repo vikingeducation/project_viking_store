29fb4d08ccbc67f4b92bbde18053d01aca2cbc5d
Visiona <darbis@gmail.com>
2017-04-22 21:13:58 +0100

    Created Edit & Show windows for Categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -15,24 +15,38 @@ class CategoriesController < ApplicationController
   def create_categ
     @category = Category.new(whitelisted_category_params)
     if @category.save
-      flash.now[:success] = "New Category has been saved"
+      flash[:success] = "New Category has been saved"
       redirect_to "/admin_portal"
     else
-      flash[:danger] = "New Category WAS NOT saved. Try again."
+      flash.now[:danger] = "New Category WAS NOT saved. Try again."
       render '/dashboard/new_categ', :locals => {:category => @category}
     end
   end
 
   def show_categ
-
+    @category = Category.find(params[:id])
+    render 'dashboard/show_categ', :locals => {:category => @category}
   end
 
   def edit_categ
     @category = Category.find(params[:id])
+    render 'dashboard/edit_categ', :locals => {:category => @category}
+  end
+
+  def update_categ
+    @category = Category.find(params[:category][:id])
+    if @category.update_attributes(whitelisted_category_params)
+      flash.now[:success] = "Category has been updated"
+      redirect_to "/admin_portal"
+    else
+      flash[:danger] = "Category hasn't been saved."
+      render '/dashboard/edit_categ', :locals => {:category => @category}
+    end
   end
 
   def delete_categ
-
+    @category = Category.find(params[:id])
+    @category.destroy
   end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
   get "/admin_portal" => 'categories#index_categ'
   get "/admin_portal/new_categ" => 'categories#new_categ'
   post "/admin_portal/create_categ" => 'categories#create_categ'
-  get "/admin_portal/show_categ" => 'categories#show_categ'
+  get "/admin_portal/:id" => 'categories#show_categ'
+  get "/admin_portal/:id/edit_categ" => 'categories#edit_categ'
+  # put "/admin_portal/:id" => 'categories#edit_categ'
+  patch "/admin_portal/:id" => 'categories#update_categ'
+  delete "/admin_portal/:id" => 'categories#delete_categ'
 end
