5d78cb833ce34628104420b6776b8a05c7dae4b4
efraimmgon <efraimmaugusto@gmail.com>
2017-05-05 17:19:54 -0400

    aggregate data: last 30 and 7 days

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,20 @@ class DashboardController < ApplicationController
 
   def index
 
+    seven_days = {
+      :users => User.where("created_at > ?", Time.now - 7.minute).count,
+      :orders => Order.where("created_at > ?", Time.now - 7.day).count,
+      :products => Product.where("created_at > ?", Time.now - 7.day).count,
+      :revenue => revenue_since(Time.now - 7.day)
+    }
+
+    thirty_days = {
+      :users => User.where("created_at > ?", Time.now - 30.day).count,
+      :orders => Order.where("created_at > ?", Time.now - 30.day).count,
+      :products => Product.where("created_at > ?", Time.now - 30.day).count,
+      :revenue => revenue_since(Time.now - 30.day)
+    }
+
     total = {
       :users => User.count,
       :orders => Order.count,
@@ -10,7 +24,9 @@ class DashboardController < ApplicationController
     }
 
     @one = {
-      :total => total
+      :total => total,
+      :thirty_days => thirty_days,
+      :seven_days => seven_days
     }
   end
 
@@ -21,6 +37,14 @@ class DashboardController < ApplicationController
       "JOIN orders ON orders.id = order_contents.order_id").joins(
       "JOIN products ON products.id = order_contents.product_id").where(
       "orders.checkout_date IS NOT NULL").sum(
+      "products.price * order_contents.quantity")
+  end
+
+  def revenue_since(d)
+    OrderContent.joins(
+      "JOIN orders ON orders.id = order_contents.order_id").joins(
+      "JOIN products ON products.id = order_contents.product_id").where(
+      "orders.checkout_date IS NOT NULL AND order_contents.created_at > ?", d).sum(
       "products.price * order_contents.quantity")
   end
 
