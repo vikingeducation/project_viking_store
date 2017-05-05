f75b38fd842a468a958a34c04197e8fcf0fe33d0
efraimmgon <efraimmaugusto@gmail.com>
2017-05-05 17:03:37 -0400

    rename variables for dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,18 +1,27 @@
 class DashboardController < ApplicationController
 
   def index
-    total_revenue = OrderContent.joins(
-      "JOIN orders ON orders.id = order_contents.order_id").joins(
-      "JOIN products ON products.id = order_contents.product_id").where(
-      "orders.checkout_date IS NOT NULL").sum(
-      "products.price * order_contents.quantity")
 
-    @total = {
+    total = {
       :users => User.count,
       :orders => Order.count,
       :products => Product.count,
       :revenue => total_revenue
     }
+
+    @one = {
+      :total => total
+    }
+  end
+
+  # Helpers
+
+  def total_revenue
+    OrderContent.joins(
+      "JOIN orders ON orders.id = order_contents.order_id").joins(
+      "JOIN products ON products.id = order_contents.product_id").where(
+      "orders.checkout_date IS NOT NULL").sum(
+      "products.price * order_contents.quantity")
   end
 
 end
