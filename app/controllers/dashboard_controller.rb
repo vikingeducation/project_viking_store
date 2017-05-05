22b321e1c0e8ab04c28654e9f9601f59971cefb8
efraimmgon <efraimmaugusto@gmail.com>
2017-05-05 16:52:36 -0400

    all time totals

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,18 @@
 class DashboardController < ApplicationController
 
   def index
+    total_revenue = OrderContent.joins(
+      "JOIN orders ON orders.id = order_contents.order_id").joins(
+      "JOIN products ON products.id = order_contents.product_id").where(
+      "orders.checkout_date IS NOT NULL").sum(
+      "products.price * order_contents.quantity")
+
+    @total = {
+      :users => User.count,
+      :orders => Order.count,
+      :products => Product.count,
+      :revenue => total_revenue
+    }
   end
-  
+
 end
