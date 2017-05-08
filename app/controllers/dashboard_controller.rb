2a1af1cc1bbde0124537e9cbb8f4264656f6bad4
efraimmgon <efraimmaugusto@gmail.com>
2017-05-08 10:17:58 -0400

    User demographics: top user with...

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -10,7 +10,8 @@ class DashboardController < ApplicationController
 
     @two = {
       :states => top_3_states_users_live,
-      :cities => top_3_cities_users_live
+      :cities => top_3_cities_users_live,
+      :top_user_with => top_user_with
     }
   end
 
@@ -79,6 +80,87 @@ class DashboardController < ApplicationController
     ).joins(
       "JOIN users u ON u.billing_id = a.id"
     ).group(:name).order("total DESC").limit(3)
+  end
+
+  def highest_order_value
+    r = Order.select(
+      "SUM(p.price) AS quantity,
+       u.first_name || ' ' || u.last_name AS customer_name,
+       orders.id AS order_id"
+    ).joins(
+      "JOIN users u ON u.id = orders.user_id"
+    ).joins(
+      "JOIN order_contents oc ON oc.order_id = orders.id"
+    ).joins(
+      "JOIN products p ON oc.product_id = p.id"
+    ).where("checkout_date IS NOT NULL").group("2, 3").order(
+      "1 DESC"
+    ).limit(1).first
+    {:column_name => "Highest Single Order Value",
+     :customer_name => r.customer_name,
+     :quantity => r.quantity}
+  end
+
+  def highest_lifetime_value
+    r = Order.select(
+      "SUM(p.price) AS quantity, u.first_name || ' ' || u.last_name AS customer_name"
+    ).joins(
+      "JOIN users u ON u.id = orders.user_id"
+    ).joins(
+      "JOIN order_contents oc ON oc.order_id = orders.id"
+    ).joins(
+      "JOIN products p ON oc.product_id = p.id"
+    ).where("checkout_date IS NOT NULL").group(:customer_name).order(
+      "1 DESC"
+    ).limit(1).first
+    {:column_name => "Highest Lifetime Value",
+     :customer_name => r.customer_name,
+     :quantity => r.quantity}
+  end
+
+  def highest_average_order_value
+    r = Order.select(
+      "AVG(p.price) AS quantity, u.first_name || ' ' || u.last_name AS customer_name"
+    ).joins(
+      "JOIN users u ON u.id = orders.user_id"
+    ).joins(
+      "JOIN order_contents oc ON oc.order_id = orders.id"
+    ).joins(
+      "JOIN products p ON oc.product_id = p.id"
+    ).where("checkout_date IS NOT NULL").group(:customer_name).order(
+      "1 DESC"
+    ).limit(1).first
+    {:column_name => "Highest Average Order Value",
+     :customer_name => r.customer_name,
+     :quantity => r.quantity}
+  end
+
+  def most_orders_placed
+    r = Order.select(
+      "COUNT(orders.user_id) AS quantity,
+       u.first_name || ' ' || u.last_name AS customer_name"
+    ).joins(
+      "JOIN users u ON u.id = orders.user_id"
+    ).joins(
+      "JOIN order_contents oc ON oc.order_id = orders.id"
+    ).joins(
+      "JOIN products p ON oc.product_id = p.id"
+    ).where("checkout_date IS NOT NULL").group(:customer_name).order(
+      "1 DESC"
+    ).limit(1).first
+    {:column_name => "Most Orders Placed",
+     :customer_name => r.customer_name,
+     :quantity => r.quantity}
+  end
+
+
+  def top_user_with
+    [
+      highest_order_value,
+      highest_lifetime_value,
+      highest_average_order_value,
+      most_orders_placed
+    ]
   end
 
 
