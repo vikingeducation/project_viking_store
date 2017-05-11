021fe4bf07da132c4d7035b2726480cc141f77bd
efraimmgon <efraimmaugusto@gmail.com>
2017-05-11 10:06:09 -0400

    Basic admin template; create admin controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "dashboard#index"
+  root "admin#index"
 
+  resources :admin
   get "/dashboard" => "dashboard#index"
 end
