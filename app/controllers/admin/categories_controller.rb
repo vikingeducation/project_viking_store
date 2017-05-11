854f6864d0098ce737c2ba18f4a028275ef39dc8
efraimmgon <efraimmaugusto@gmail.com>
2017-05-11 16:51:36 -0400

    category: add view error msgs

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -10,7 +10,26 @@ class Admin::CategoriesController < ApplicationController
     @category = Category.new
   end
 
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:success] = "New category created"
+      redirect_to admin_categories_path
+    else
+      flash[:danger] = "Failed to create category"
+      render :new
+    end
+  end
+
   def show
+  end
+
+  # -----------------------------------------------------------------
+  # Helpers
+  # -----------------------------------------------------------------
+
+  def category_params
+    params.require(:category).permit(:name, :description)
   end
 
 end
