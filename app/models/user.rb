f4b8b61ffe9c4b95e4affbe416a16666889a34cb
andie5 <andieasa@googlemail.com>
2017-08-06 01:14:34 +0100

    created queries for first 2 sections of the dashboard updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,13 +14,53 @@ class User < ApplicationRecord
 
   def highest_single_order_value
     User.find_by_sql("
-      SELECT SUM(price * quantity) AS order_value, users.first_name, orders.id FROM orders
+      SELECT price * quantity AS order_value, users.first_name, orders.id, users.last_name FROM orders
       JOIN order_contents ON orders.id = order_contents.order_id
       JOIN products on products.id = order_contents.product_id
       JOIN users on orders.user_id = users.id
       WHERE checkout_date IS NOT NULL
-      GROUP BY orders.id, users.first_name
       ORDER BY order_value DESC
+      LIMIT 1;
+      ")
+
+  end
+
+  def highest_lifetime_value
+    User.find_by_sql("
+      SELECT SUM(price * quantity) AS order_value, users.first_name, orders.id, users.last_name FROM orders
+      JOIN order_contents ON orders.id = order_contents.order_id
+      JOIN products on products.id = order_contents.product_id
+      JOIN users on orders.user_id = users.id
+      WHERE checkout_date IS NOT NULL
+      GROUP BY orders.id, users.first_name, users.last_name
+      ORDER BY order_value DESC
+      LIMIT 1;
+      ")
+
+  end
+
+   def highest_average_order
+    User.find_by_sql("
+      SELECT AVG(price * quantity) AS order_value, users.first_name, orders.id, users.last_name FROM orders
+      JOIN order_contents ON orders.id = order_contents.order_id
+      JOIN products on products.id = order_contents.product_id
+      JOIN users on orders.user_id = users.id
+      WHERE checkout_date IS NOT NULL
+      GROUP BY orders.id, users.first_name, users.last_name
+      ORDER BY order_value DESC
+      LIMIT 1;
+      ")
+
+  end
+
+  def most_orders_placed
+    User.find_by_sql("
+      SELECT COUNT(orders.id) AS order_count, users.first_name, users.last_name FROM orders
+      JOIN order_contents ON orders.id = order_contents.order_id
+      JOIN users on orders.user_id = users.id
+      WHERE checkout_date IS NOT NULL
+      GROUP BY users.first_name, users.last_name
+      ORDER BY order_count DESC
       LIMIT 1;
       ")
 
