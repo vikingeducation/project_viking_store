9a60c6f951da7702046b1f519cbee943f8055658
Roy Chen <roychenlei@gmail.com>
2017-08-17 12:51:05 +0800

    modified All Time Totals table to use only local variables

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,9 @@
 class DashboardController < ApplicationController
   def home
     render locals: {
+      total_users: User.count,
+      total_orders: Order.count,
+      total_products: Product.count,
       total_revenue: OrderContent.total_revenue
     }
   end
