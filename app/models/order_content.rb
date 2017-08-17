8255827bceff1ec1d73c37fcfb3ec40278ae0659
Roy Chen <roychenlei@gmail.com>
2017-08-17 13:29:55 +0800

    made column reference explicit

--- app/models/order_content.rb
+++ app/models/order_content.rb
@@ -2,7 +2,7 @@ class OrderContent < ApplicationRecord
   # calculates the total revenue over all time
   def self.total_revenue
     OrderContent
-    .where("checkout_date IS NOT NULL")
+    .where("orders.checkout_date IS NOT NULL")
     .joins("JOIN orders ON orders.id = order_contents.order_id")
     .joins("JOIN products ON products.id = order_contents.product_id")
     .sum("order_contents.quantity * products.price")
