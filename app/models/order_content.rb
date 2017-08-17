00bc8fd9e16e3dc50c19f2671bba6fa9e26f997f
Roy Chen <roychenlei@gmail.com>
2017-08-17 12:28:47 +0800

    added class method to calculate total revenue

--- app/models/order_content.rb
+++ app/models/order_content.rb
@@ -1,2 +1,9 @@
 class OrderContent < ApplicationRecord
+  def self.total_revenue
+    OrderContent
+    .where("checkout_date IS NOT NULL")
+    .joins("JOIN orders ON orders.id = order_contents.order_id")
+    .joins("JOIN products ON products.id = order_contents.product_id")
+    .sum("order_contents.quantity * products.price")
+  end
 end
