0ad4dfe593d6980538ef36e15b2ca6301150b8f6
Roy Chen <roychenlei@gmail.com>
2017-08-18 09:57:21 +0800

    completed Total Statistics story

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,24 +4,26 @@ class DashboardController < ApplicationController
       total_users: User.count,
       total_orders: Order.count,
       total_products: Product.count,
-      total_revenue: OrderContent.total_revenue,
+      total_revenue: Order.total_revenue,
 
       new_users_within_30_days: User.new_users(30),
       orders_placed_within_30_days: Order.orders_placed(30),
       new_products_within_30_days: Product.new_products(30),
-      revenue_within_30_days: OrderContent.revenue(30),
+      revenue_within_30_days: Order.revenue(30),
 
       new_users_within_7_days: User.new_users(7),
       orders_placed_within_7_days: Order.orders_placed(7),
       new_products_within_7_days: Product.new_products(7),
-      revenue_within_7_days: OrderContent.revenue(7),
+      revenue_within_7_days: Order.revenue(7),
 
       top_3_states_by_billing_address: State.top_3_states_by_billing_address,
       top_3_cities_by_billing_address: City.top_3_cities_by_billing_address,
-      highest_single_order_value: OrderContent.highest_single_order_value,
-      highest_lifetime_value: OrderContent.highest_lifetime_value,
-      highest_average_order_value: OrderContent.highest_average_order_value,
-      user_with_most_orders_placed: Order.user_with_most_orders_placed
+      highest_single_order_value: Order.highest_single_order_value,
+      highest_lifetime_value: Order.highest_lifetime_value,
+      highest_average_order_value: Order.highest_average_order_value,
+      user_with_most_orders_placed: Order.user_with_most_orders_placed,
+
+      total_average_order_value: Order.total_average_order_value
     }
   end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,5 @@
 class Order < ApplicationRecord
-  # calculates the number of new Orders that were placed within a number of
-  # days from the current day
+  # calculates the number of new Orders that were placed within a number of days from the current day
   def self.orders_placed(within_days)
     Order
     .where("checkout_date IS NOT NULL AND created_at >= ? ", Time.now - within_days.days)
@@ -17,5 +16,83 @@ class Order < ApplicationRecord
     .order("order_count DESC")
     .limit(1)
     .first
+  end
+
+  # finds the total number of Orders placed
+  def self.total_orders
+    Order.where("checkout_date IS NOT NULL").count
+  end
+
+  # calculates the total revenue over all time
+  def self.total_revenue
+    OrderContent
+    .where("orders.checkout_date IS NOT NULL")
+    .joins("JOIN orders ON orders.id = order_contents.order_id")
+    .joins("JOIN products ON products.id = order_contents.product_id")
+    .sum("order_contents.quantity * products.price")
+  end
+
+  # calculates the revenue within a number of days from the current day
+  def self.revenue(within_days)
+    OrderContent
+    .where("orders.checkout_date IS NOT NULL AND order_contents.created_at >= ?", Time.now - within_days.days)
+    .joins("JOIN orders ON orders.id = order_contents.order_id")
+    .joins("JOIN products ON products.id = order_contents.product_id")
+    .sum("order_contents.quantity * products.price")
+  end
+
+  # calculates the revenue per Order.
+  def self.revenue_per_order
+    OrderContent
+    .joins("JOIN orders ON orders.id = order_contents.order_id")
+    .joins("JOIN products ON products.id = order_contents.product_id")
+    .joins("JOIN users ON orders.user_id = users.id")
+    .select("orders.id, users.first_name, users.last_name, SUM(order_contents.quantity * products.price) AS revenue")
+    .where("orders.checkout_date IS NOT NULL")
+    .group("orders.id, users.first_name, users.last_name")
+  end
+
+  # calculates the total revenue for each User.
+  def self.total_revenue_per_user
+    OrderContent
+    .joins("JOIN orders ON orders.id = order_contents.order_id")
+    .joins("JOIN products ON products.id = order_contents.product_id")
+    .joins("JOIN users ON orders.user_id = users.id")
+    .select("users.id, users.first_name, users.last_name, SUM(order_contents.quantity * products.price) AS revenue")
+    .where("orders.checkout_date IS NOT NULL")
+    .group("users.id")
+  end
+
+  # finds the User with the highest single Order value.
+  def self.highest_single_order_value
+    self.revenue_per_order.order("revenue DESC").limit(1).first
+  end
+
+  # finds the User with the highest lifetime revenue value.
+  def self.highest_lifetime_value
+    self.total_revenue_per_user.order("revenue DESC").limit(1).first
+  end
+
+  # finds the User with the highest average Order value.
+  def self.highest_average_order_value
+    OrderContent.find_by_sql(
+      "
+      SELECT users.first_name, users.last_name, ROUND(SUM(order_contents.quantity * products.price) / (SELECT COUNT(orders.id) FROM orders WHERE orders.checkout_date IS NOT NULL AND orders.user_id = users.id), 2) AS average_order_value
+      FROM users
+      JOIN orders ON orders.user_id = users.id
+      JOIN order_contents ON order_contents.order_id = orders.id
+      JOIN products ON products.id = order_contents.product_id
+      WHERE orders.checkout_date IS NOT NULL
+      GROUP BY users.id
+      ORDER BY average_order_value DESC
+      LIMIT 1;
+      "
+    ).first
+  end
+
+  # determines the average Order value across all time
+  def self.total_average_order_value
+    # total revenue / total number of orders
+    self.total_revenue / self.total_orders
   end
 end
