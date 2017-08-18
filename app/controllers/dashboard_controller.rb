53559c400e9b46c6708d094f0e2963b247048703
Roy Chen <roychenlei@gmail.com>
2017-08-18 10:03:37 +0800

    renamed method

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -7,12 +7,12 @@ class DashboardController < ApplicationController
       total_revenue: Order.total_revenue,
 
       new_users_within_30_days: User.new_users(30),
-      orders_placed_within_30_days: Order.orders_placed(30),
+      orders_placed_within_30_days: Order.orders_placed_within(30),
       new_products_within_30_days: Product.new_products(30),
       revenue_within_30_days: Order.revenue_within(30),
 
       new_users_within_7_days: User.new_users(7),
-      orders_placed_within_7_days: Order.orders_placed(7),
+      orders_placed_within_7_days: Order.orders_placed_within(7),
       new_products_within_7_days: Product.new_products(7),
       revenue_within_7_days: Order.revenue_within(7),
 
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,8 +1,8 @@
 class Order < ApplicationRecord
   # calculates the number of new Orders that were placed within a number of days from the current day
-  def self.orders_placed(within_days)
+  def self.orders_placed_within(days)
     Order
-    .where("checkout_date IS NOT NULL AND created_at >= ? ", Time.now - within_days.days)
+    .where("checkout_date IS NOT NULL AND created_at >= ? ", Time.now - days.days)
     .count
   end
 
