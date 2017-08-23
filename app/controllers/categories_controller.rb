1a7636ab0a723286e2f0719a53434b1a96a99642
Roy Chen <roychenlei@gmail.com>
2017-08-23 12:12:33 +0800

    created Categories index page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,8 +2,6 @@ class CategoriesController < ApplicationController
   def index
     @categories = Category.all
 
-    flash.now[:notify] = "Displaying Product Categories.."
-
     render layout: "admin_portal"
   end
 end
