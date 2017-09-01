4197c88cdfdb05571f747fa0c2485e127fa83b3a
andie5 <andieasa@googlemail.com>
2017-09-01 01:41:55 +0100

    epic 2 - started setting up crud actions for categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,4 +3,38 @@ class CategoriesController < ApplicationController
   def index
     @categories = Category.all
   end
+
+  def new
+    @category = Category.new
+  end
+
+  def show
+    @category = Category.find(params[:id])
+  end
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update_attributes(category_form_params)
+      redirect_to category_path(@category)
+    else
+      render :show
+    end
+  end
+
+  def create
+    @category = Category.new(category_form_params)
+    if @category.save
+      redirect_to category_path(@category)
+    else
+      render :show
+    end
+  end
+
+  def category_form_params
+    params.require(:category).permit(:name, :description, :created_at, :updated_at)
+  end
 end
