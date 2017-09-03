83d67c5b2642f044f3ce25f0fa390570378c0345
Roy Chen <roychenlei@gmail.com>
2017-09-03 10:09:21 +0800

    added display of all Addresses for a specific User

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,6 +1,16 @@
 class AddressesController < ApplicationController
   def index
-    @addresses = Address.all
+    if params[:user_id]
+      if User.exists?(params[:user_id])
+        @addresses = Address.where(user_id: params[:user_id])
+        @user = User.find(params[:user_id])
+      else
+        flash.now[:error] = "Invalid User ID provided. Displaying all Addresses.."
+        @addresses = Address.all
+      end
+    else
+      @addresses = Address.all
+    end
 
     render layout: "admin_portal"
   end
