0c447ba76e1592318e7cfc25b1211b8931d4250c
Sean Luckett <whitecrane007@gmail.com>
2017-10-02 09:57:24 -0600

    Get new users in last n days

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+  scope :signed_up_since, ->(begin_date) {
+    where('created_at > ? AND created_at < ?', begin_date, Time.zone.now)
+  }
+
+  def self.signed_up_in_last(num_days)
+    begin_date = eval("#{num_days}.days.ago")
+    signed_up_since(begin_date)
+  end
 end
