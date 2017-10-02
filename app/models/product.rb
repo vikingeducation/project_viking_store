e9c2a47bdfa70fbd6bc083624c7d934b44ac4520
Sean Luckett <whitecrane007@gmail.com>
2017-10-02 10:00:43 -0600

    Get products listed in last n days

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,10 @@
 class Product < ApplicationRecord
+  scope :listed_since, ->(begin_date) {
+    where('created_at > ? AND created_at < ?', begin_date, Time.zone.now)
+  }
+
+  def self.listed_in_last(num_days)
+    begin_date = eval("#{num_days}.days.ago")
+    listed_since(begin_date)
+  end
 end
