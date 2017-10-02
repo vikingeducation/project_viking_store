9adcd8a42f9a0d6866aaa7f0aff79ff929c990e9
Sean Luckett <whitecrane007@gmail.com>
2017-10-02 09:49:22 -0600

    Get orders placed in last n days

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,11 @@
 class Order < ApplicationRecord
+  scope :placed, -> { where.not(checkout_date: nil) }
+  scope :placed_since, ->(begin_date) {
+    placed.where('checkout_date > ? AND checkout_date < ?', begin_date, Time.zone.now)
+  }
+
+  def self.placed_in_last(num_days)
+    begin_date = eval("#{num_days}.days.ago")
+    placed_since(begin_date)
+  end
 end
