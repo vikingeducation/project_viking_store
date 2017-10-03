29c1bb2b035351579b48c6e377a740621e8aac15
Sean Luckett <whitecrane007@gmail.com>
2017-10-03 14:45:12 -0600

    Get top n lived-in states based on billing address

--- app/models/state.rb
+++ app/models/state.rb
@@ -1,2 +1,10 @@
 class State < ApplicationRecord
+  def self.most_lived_in(num_states)
+    select('states.name, COUNT(users.billing_id) as address_count')
+      .joins('JOIN addresses ON states.id = addresses.state_id')
+      .joins('JOIN users ON addresses.user_id = users.id')
+      .group('states.name')
+      .order('address_count DESC')
+      .limit(num_states)
+  end
 end
