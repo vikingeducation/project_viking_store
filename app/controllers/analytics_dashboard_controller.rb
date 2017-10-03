caf9f81f04fdde7e8fa90a1530d1eb041deec1cd
Sean Luckett <whitecrane007@gmail.com>
2017-10-03 09:55:07 -0600

    Adds revenue calculations by number of days since today
    The overall platform data revenue was originally total revenue. In
    addition to total, revenue can now break into number of days passed.
    Finally, the revenue calculation is now on Order model where it makes
    more sense.

--- app/controllers/analytics_dashboard_controller.rb
+++ app/controllers/analytics_dashboard_controller.rb
@@ -1,17 +1,18 @@
 class AnalyticsDashboardController < ApplicationController
   def index
-    @revenue = Product.revenue
-
     @new_users_7 = User.signed_up_in_last 7
     @orders_7 = Order.placed_in_last 7
     @new_products_7 = Product.listed_in_last 7
+    @revenue_7 = Order.revenue_since 7
 
     @new_users_30 = User.signed_up_in_last 30
     @orders_30 = Order.placed_in_last 30
     @new_products_30 = Product.listed_in_last 30
+    @revenue_30 = Order.revenue_since 30
 
     @users_total = User.all
     @orders_total = Order.all
     @products_total = Product.all
+    @revenue = Order.revenue
   end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -4,8 +4,29 @@ class Order < ApplicationRecord
     placed.where('checkout_date > ? AND checkout_date < ?', begin_date, Time.zone.now)
   }
 
+  scope :product_orders, -> {
+    joins('JOIN order_contents ON orders.id = order_contents.order_id')
+      .joins('JOIN products ON order_contents.product_id = products.id')
+  }
+
   def self.placed_in_last(num_days)
     begin_date = eval("#{num_days}.days.ago")
     placed_since(begin_date)
+  end
+
+  def self.revenue
+    product_orders
+      .sum('products.price * order_contents.quantity')
+      .to_f
+  end
+
+  def self.revenue_since(num_days)
+    begin_date = eval("#{num_days}.days.ago")
+
+    product_orders
+      .where('checkout_date > ? AND checkout_date < ?',
+             begin_date, Time.zone.now)
+      .sum('products.price * order_contents.quantity')
+      .to_f
   end
 end
