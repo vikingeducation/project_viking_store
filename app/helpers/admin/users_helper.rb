024aadd225c010f43b2e1f0ed18f6227aaa76f29
Mariah Schneeberger <mariah.acacia@gmail.com>
2017-11-03 18:05:45 -0700

    finished user index page

--- app/helpers/admin/users_helper.rb
+++ app/helpers/admin/users_helper.rb
@@ -1,11 +1,35 @@
 module Admin::UsersHelper
 
-  def user_state(user_id)
-    State.select(:name).joins(:addresses).where("addresses.user_id" => user_id).first
+  def user_state(users_id)
+    State.select(:name).
+          joins(:addresses).
+          where("addresses.user_id" => users_id).
+          first
   end
 
-  def user_city(user_id)
-    City.select(:name).joins(:addresses).where("addresses.user_id" => user_id).first
+  def user_city(users_id)
+    City.select(:name).
+         joins(:addresses).
+         where("addresses.user_id" => users_id).
+         first
+  end
+
+  def user_orders(users_id)
+    Order.where(user_id: users_id).
+          group(:user_id).
+          where.not(checkout_date: nil).
+          count.
+          values.
+          first
+  end
+
+  def user_last_order(users_id)
+    @order = Order.where(user_id: users_id).
+                   group(:user_id, "orders.id").
+                   where.not(checkout_date: nil).
+                   order(checkout_date: :desc).
+                   first
+    @order.nil? ? "N/A" : @order[:checkout_date].strftime("%F")
   end
 
 end
