933083bd5260d11924876730e8012f5c3101faf1
Jacob Wald <email@example.com>
2017-11-09 13:49:36 -0500

    add view file structure for analysis

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,11 +1,11 @@
 class DashboardsController < ApplicationController
   def index
-    @overall_analysis = overall_analysis
+    @platform_analysis = platform_analysis
   end
 
   private
 
-  def overall_analysis
+  def platform_analysis
     [7.days.ago, 30.days.ago, Time.at(0)].map do |date|
       PlatformAnalysis.new(from_day: date)
     end
