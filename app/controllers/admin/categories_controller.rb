42c8d8cc08aee39f35e3410cb9871b75f9c10538
Jacob Wald <schoen575@gmail.com>
2017-11-19 16:04:58 -0500

    add admin template

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,5 @@
+module Admin
+  class CategoriesController < ApplicationController
+    layout "admin"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   namespace :admin do
     get "/dashboard", to: "dashboards#index"
+    resources :categories
   end
 end
