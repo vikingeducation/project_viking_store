e93226a772404dbf1b77fd443596d53cf3fa1ff9
anne <richardson.ae@gmail.com>
2017-12-05 18:01:58 -0600

    Adds top 3 query

--- app/models/city.rb
+++ app/models/city.rb
@@ -2,4 +2,12 @@ class City < ApplicationRecord
 
   has_many :addresses
 
+  def self.three_with_most_users
+    select("cities.name AS name, COUNT(*) AS user_count").
+    joins("JOIN addresses a ON cities.id = a.city_id JOIN users u ON u.billing_id = a.id").
+    order("user_count DESC").
+    group("cities.name").
+    limit(3)
+  end
+
 end
--- app/models/state.rb
+++ app/models/state.rb
@@ -2,4 +2,12 @@ class State < ApplicationRecord
 
   has_many :addresses
 
+  def self.three_with_most_users
+    select("states.name AS name, COUNT(*) AS user_count").
+    joins("JOIN addresses a ON states.id = a.state_id JOIN users u ON u.billing_id = a.id").
+    order("user_count DESC").
+    group("states.name").
+    limit(3)
+  end
+
 end
