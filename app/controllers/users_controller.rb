cbb60c8af4bc0f56cc7f410f3b3ed9e71158b591
anne <richardson.ae@gmail.com>
2017-12-07 11:25:12 -0600

    Adds link for new user and sorts by desc date

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   def index
-    @users = User.all
+    @users = User.all.order(created_at: :desc)
   end
 
   def new
