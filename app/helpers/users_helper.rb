13953127b8d6a329abd845af9ee079a23df1340c
anne <richardson.ae@gmail.com>
2017-12-07 11:00:54 -0600

    Adds user details

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,11 @@
 module UsersHelper
+
+  def address(add)
+    base = "#{add.city.name}, #{add.state.name} #{add.zip_code}"
+    if add.secondary_address.blank?
+      "#{add.street_address}, " + base
+    else
+      "#{add.street_address}, #{add.secondary_address}," + base
+    end
+  end
 end
